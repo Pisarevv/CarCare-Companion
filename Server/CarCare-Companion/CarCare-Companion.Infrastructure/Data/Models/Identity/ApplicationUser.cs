1582301140d6ae1f7190b8ea1491928aebcfd83c
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-06-19 16:22:31 +0300

    Added audit info to ApplicationUser

--- Server/CarCare-Companion/CarCare-Companion.Infrastructure/Data/Models/Identity/ApplicationUser.cs
+++ Server/CarCare-Companion/CarCare-Companion.Infrastructure/Data/Models/Identity/ApplicationUser.cs
@@ -3,25 +3,34 @@
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 
+using CarCare_Companion.Infrastructure.Data.Models.Contracts;
 using CarCare_Companion.Infrastructure.Data.Models.Vehicle;
 
 using static CarCare_Companion.Common.ValidationConstants.ApplicationUser;
+using Microsoft.EntityFrameworkCore;
 
-public class ApplicationUser : IdentityUser<Guid>
+public class ApplicationUser : IdentityUser<Guid>, IAuditInfo
 {
     public ApplicationUser()
     {
        this.Vehicles = new HashSet<Vehicle>(); 
     }
 
+    [Comment("User first name")]
     [MinLength(MinFirstNameLength)]
     [MaxLength(MaxFirstNameLength)]
     public string? FirstName { get; set; }
 
+    [Comment("User last name")]
     [MinLength(MinLastNameLength)]
     [MaxLength(MaxLastNameLength)]
     public string? LastName { get; set;}
 
-    public ICollection<Vehicle> Vehicles { get; set; }
+    [Comment("Date of user creation")]
+    public DateTime CreatedOn { get; set; }
 
+    [Comment("Last date of user modification")]
+    public DateTime? ModifiedOn { get; set ; }
+
+    public ICollection<Vehicle> Vehicles { get; set; }
 }
