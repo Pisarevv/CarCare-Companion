9456de73690762516733d8861033160149ca4e8f
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-06-21 10:08:03 +0300

    Communication between api and react test

--- Client/CarCare-Companion/src/App.jsx
+++ Client/CarCare-Companion/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState("")
+
+  useEffect(() => {
+    (async () => {
+    var result = await fetch("https://localhost:7152/Home");
+    var result2 = await result.json();
+    setCount(count => result2.message);
+    })()
+  },[])
 
   return (
     <>
-     
+     <h1>{count}</h1>
     </>
   )
 }
