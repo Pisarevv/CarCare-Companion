b8f4988333d58a2c0ea8f4f62947daee0db8ef44
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-06-24 18:55:18 +0300

    Added authentication data model

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Identity/AuthDataModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CarCare_Companion.Core.Models.Identity;
+
+public class AuthDataModel
+{
+    public string Token { init; get; } = null!;
+
+    public string Email { init; get; } = null!;
+
+    public string Id { init; get; } = null!;
+
+}
