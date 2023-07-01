a11b090bc74d4dc3c12be9dd2355cf9e6c1fae53
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-01 15:14:13 +0300

    AddVehicle page inputs and css update

--- Client/CarCare-Companion/src/components/AddVehicle/AddVehicle.jsx
+++ Client/CarCare-Companion/src/components/AddVehicle/AddVehicle.jsx
@@ -63,14 +63,14 @@ const AddVehicle = () => {
 
   const [userVehicle, dispatch] = useReducer(userVehicleReducer, {
     make: "",
-    milage: "",
+    mileage: "",
     fuelType: "",
     model: "",
     type: "",
     year: "",
 
     makeError: "",
-    milageError: "",
+    mileageError: "",
     fuelTypeError: "",
     modelError: "",
     typeError: "",
@@ -125,18 +125,18 @@ const AddVehicle = () => {
     e.preventDefault();
     try {
       let isMakeValid = validateTextFields("make", userVehicle.make);
-      let isMilageValid = validateTextFields("milage", userVehicle.milage);
+      let isMileageValid = validateTextFields("mileage", userVehicle.mileage);
       let isFuelTypeValid = validateTextFields("fuelType", userVehicle.fuelType);
       let isModelValid = validateTextFields("model", userVehicle.model);
       let isTypeValid = validateTextFields("type", userVehicle.type);
       let isYearValid = validateNumberFields("year", userVehicle.year);
 
-      if (isMakeValid && isMilageValid &&
+      if (isMakeValid && isMileageValid &&
         isFuelTypeValid && isModelValid &&
         isTypeValid && isYearValid
       ) {
-        let { make, milage, fuelType, model, type, year } = userVehicle;
-        // await createuserVehicle({ make, milage, fuelType, model, price, type, year });
+        let { make, mileage, fuelType, model, type, year } = userVehicle;
+        // await createuserVehicle({ make, mileage, fuelType, model, price, type, year });
         // navigate("/recycle/page/1");
 
       }
@@ -145,6 +145,7 @@ const AddVehicle = () => {
       }
 
     } catch (error) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
       ErrorHandler(error)
     }
   }
@@ -156,35 +157,37 @@ const AddVehicle = () => {
           <div className="add-vehicle-heading">
             <h2>Add vehicle</h2>
             <form onSubmit={onVehicleAdd}>
-              <div className="input-group input-group-lg">
-                <label>Image:</label>
-                {vehicleImage && <img src={vehicleImage} alt="Selected" />}
-                <input className="form-control" type="file" accept="image/*" onChange={handleImageUpload} />                       
+              <div className="input-group input-group-lg img-group">
+                <label>Image: (Optional)</label>
+                {vehicleImage && <img className="vehicle-image" src={vehicleImage} alt="Selected" />}
+                <label className="select-img-button" htmlFor="file" >Select Image</label> 
+                <input className="form-control" type="file" accept="image/*" id="file" onChange={handleImageUpload} />     
+                                
               </div>
               <div className="input-group input-group-lg">
-                <label>make:</label>
-                <input className="form-control" type="text" placeholder="make" name="make" value={userVehicle.make} onChange={onInputChange} />
-                {userVehicle.makeError && <p>{userVehicle.makeError}</p>}
-              </div>
-              <div className="input-group input-group-lg">
-                <label>milage:</label>
-                <input className="form-control" type="text" placeholder="milage" name="milage" value={userVehicle.milage} onChange={onInputChange} />
-                {userVehicle.milageError && <p>{userVehicle.milageError}</p>}
+                <label>Make:</label>
+                <input className="form-control" type="text" placeholder="Vehicle make" name="make" value={userVehicle.make} onChange={onInputChange} />
+                {userVehicle.makeError && <p className="invalid-field" >{userVehicle.makeError}</p>}
               </div>
               <div className="input-group input-group-lg">
                 <label>Model:</label>
                 <input className="form-control" type="text" placeholder="Model" name="model" value={userVehicle.model} onChange={onInputChange} />
-                {userVehicle.modelError && <p>{userVehicle.modelError}</p>}
+                {userVehicle.modelError && <p className="invalid-field">{userVehicle.modelError}</p>}
+              </div>
+              <div className="input-group input-group-lg">
+                <label>Mileage:</label>
+                <input className="form-control" type="text" placeholder="Mileage" name="mileage" value={userVehicle.mileage} onChange={onInputChange} />
+                {userVehicle.mileageError && <p className="invalid-field" >{userVehicle.mileageError}</p>}
               </div>
               <div className="input-group input-group-lg">
                 <label>Vehicle type:</label>
-                <input className="form-control" type="text" placeholder="Product type" name="type" value={userVehicle.type} onChange={onInputChange} />
-                {userVehicle.typeError && <p>{userVehicle.typeError}</p>}
+                <input className="form-control" type="text" placeholder="Vehicle type" name="type" value={userVehicle.type} onChange={onInputChange} />
+                {userVehicle.typeError && <p className="invalid-field">{userVehicle.typeError}</p>}
               </div>
               <div className="input-group input-group-lg">
                 <label>Year:</label>
                 <input className="form-control" type="text" placeholder="Year" name="year" value={userVehicle.year} onChange={onInputChange} />
-                {userVehicle.yearError && <p>{userVehicle.yearError}</p>}
+                {userVehicle.yearError && <p className="invalid-field">{userVehicle.yearError}</p>}
               </div>
               <button type="submit" className="float">Add vehicle</button>
               <NavLink className="float" to={`/recycle/page/1`}>Cancel</NavLink>
