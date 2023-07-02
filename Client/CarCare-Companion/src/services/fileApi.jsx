fd647e4d331ec69be463f1d65e9942f4586aa516
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-02 20:18:01 +0300

    Added additional api service for files

---
+++ Client/CarCare-Companion/src/services/fileApi.jsx
@@ -0,0 +1,69 @@
+let target = "https://localhost:7152"
+
+async function request(method, url, data, vehicleId) {
+    let options = {
+        method,
+        headers: {}
+    }
+
+    options.headers["vehicleId"] = vehicleId;
+    options.body = data;
+    
+    const user = localStorage.getItem('user');
+    const auth = JSON.parse(user || '{}');
+
+
+    if (auth.accessToken) {
+        options.headers['Authorization'] = "Bearer " + auth.accessToken;
+    }
+
+    try {
+        let response = await fetch(target + url, options);
+
+        if (response.status === 204) {
+            return response;
+        }
+
+        let result = await response.json();
+
+        if (response.ok !== true) {
+            throw (result.title);
+        }
+
+        return result;
+    }
+    catch (error) {
+        throw (error);
+    }
+
+}
+
+
+
+function post(url, data, contentType) {
+    return request("POST", url, data, contentType)
+}
+
+function get(url) {
+    return request("GET", url);
+}
+
+function put(url, data, contentType) {
+    return request("PUT", url, data, contentType)
+}
+
+function patch(url, data, contentType) {
+    return request("PATCH", url, data, contentType)
+}
+
+function del(url) {
+    return request("delete", url);
+}
+
+export {
+    post,
+    get,
+    put,
+    patch,
+    del as delete
+}
