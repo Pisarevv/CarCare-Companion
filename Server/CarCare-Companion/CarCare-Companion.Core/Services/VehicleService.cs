9864b842d1b0fbb05518f9d8a819345fc536ee88
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-02 20:17:31 +0300

    Added create vehicle functionality

--- Server/CarCare-Companion/CarCare-Companion.Core/Services/VehicleService.cs
+++ Server/CarCare-Companion/CarCare-Companion.Core/Services/VehicleService.cs
@@ -20,10 +20,65 @@ public class VehicleService : IVehicleService
         this.repository = repository;
     }
 
-    public async Task<ICollection<FuelTypeRequestModel>> GetAllFuelTypesAsync()
+    public async Task AddImageToVehicle(string vehicleId, string imageId)
+    {
+        Vehicle vehicle = await repository.GetByIdAsync<Vehicle>(Guid.Parse(vehicleId));
+        if (vehicle != null)
+        {
+           vehicle.VehicleImageKey = Guid.Parse(imageId);
+           await  repository.SaveChangesAsync();  
+        }
+    }
+
+    public async Task<string> CreateVehicleAsync(VehicleCreateRequestModel model)
+    {
+        Vehicle newVehicle = new Vehicle()
+        {
+            Make = model.Make,
+            Model = model.Model,
+            Mileage = model.Mileage,
+            FuelTypeId = model.FuelTypeId,
+            VehicleTypeId = model.VehicleTypeId,
+            Year = model.Year,
+            OwnerId = model.OwnerId,
+            CreatedOn = DateTime.UtcNow
+        };
+
+        await repository.AddAsync(newVehicle);
+        await repository.SaveChangesAsync();
+
+        return newVehicle.Id.ToString();
+    }
+
+    public async Task<bool> DoesFuelTypeExistAsync(int id)
+    {
+        var result = await repository.GetByIdAsync<FuelType>(id);
+
+        if(result !=  null)
+        {
+            return true;
+        }
+
+        return false;
+               
+    }
+
+    public async Task<bool> DoesVehicleTypeExistAsync(int id)
+    {
+        var result = await repository.GetByIdAsync<VehicleType>(id);
+
+        if (result != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public async Task<ICollection<FuelTypeResponseModel>> GetAllFuelTypesAsync()
     {
         return await repository.AllReadonly<FuelType>()
-               .Select(ft => new FuelTypeRequestModel
+               .Select(ft => new FuelTypeResponseModel
                {
                    Id = ft.Id,
                    Name = ft.Name,
@@ -31,10 +86,10 @@ public class VehicleService : IVehicleService
                .ToListAsync();
     }
 
-    public async Task<ICollection<VehicleTypeRequestModel>> GetAllVehicleTypesAsync()
+    public async Task<ICollection<VehicleTypeResponseModel>> GetAllVehicleTypesAsync()
     {
         return await repository.AllReadonly<VehicleType>()
-             .Select(ft => new VehicleTypeRequestModel
+             .Select(ft => new VehicleTypeResponseModel
              {
                  Id = ft.Id,
                  Name = ft.Name,
