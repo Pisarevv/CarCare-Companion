dc7a13a9bac4dc7108be66fecac55554c57c2bd5
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-02 15:16:14 +0300

    Added request models

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Vehicle/FuelTypeRequestModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CarCare_Companion.Core.Models.Vehicle;
+
+
+public class FuelTypeRequestModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+}
---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Vehicle/VehicleTypeRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CarCare_Companion.Core.Models.Vehicle;
+
+public class VehicleTypeRequestModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+}
