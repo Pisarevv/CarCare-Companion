59774654bf7c19100bd4632c24414b6e431b1a47
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-02 20:19:38 +0300

    Added methods for vehicles
    Creating a vehicle.
    Adding an image to it.

--- Server/CarCare-Companion/CarCare-Companion.Api/Controllers/VehicleController.cs
+++ Server/CarCare-Companion/CarCare-Companion.Api/Controllers/VehicleController.cs
@@ -5,20 +5,23 @@ using Microsoft.Data.SqlClient;
 
 using CarCare_Companion.Core.Contracts;
 using CarCare_Companion.Core.Models.Status;
-
+using CarCare_Companion.Core.Models.Vehicle;
 
 using static CarCare_Companion.Common.StatusResponses;
-using CarCare_Companion.Core.Models.Vehicle;
+using static Common.FileMethods;
+
 
 [Route("[/Vehicle]")]
 public class VehicleController : BaseController
 {
     private readonly IVehicleService vehicleService;
+    private readonly IImageService imageService;
     private readonly ILogger<VehicleController> logger;
 
-    public VehicleController(IVehicleService vehicleService, ILogger<VehicleController> logger)
+    public VehicleController(IVehicleService vehicleService, IImageService imageService, ILogger<VehicleController> logger)
     {
         this.vehicleService = vehicleService;
+        this.imageService = imageService;
         this.logger = logger;
     }
 
@@ -29,8 +32,7 @@ public class VehicleController : BaseController
     {
         try
         {
-            ICollection<FuelTypeRequestModel> fuelTypes = await vehicleService.GetAllFuelTypesAsync();
-
+            ICollection<FuelTypeResponseModel> fuelTypes = await vehicleService.GetAllFuelTypesAsync();
             return StatusCode(200, fuelTypes);
         }
         catch (SqlException ex)
@@ -52,7 +54,7 @@ public class VehicleController : BaseController
     {
         try
         {
-            ICollection<VehicleTypeRequestModel> vehicleTypes = await vehicleService.GetAllVehicleTypesAsync();
+            ICollection<VehicleTypeResponseModel> vehicleTypes = await vehicleService.GetAllVehicleTypesAsync();
 
             return StatusCode(200, vehicleTypes);
         }
@@ -67,4 +69,53 @@ public class VehicleController : BaseController
             return StatusCode(403, new StatusErrorInformation(InvalidData));
         }
     }
+
+    [HttpPost]
+    [Route("/VehicleCreate")]
+    [Produces("application/json")]
+    public async Task<IActionResult> CreateVehicle(VehicleCreateRequestModel model)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+            {
+                return StatusCode(400, new
+                {
+                    title = MissingOrInvalidFields,
+                });
+            }
+            string vehicleId = await vehicleService.CreateVehicleAsync(model);
+          
+            return StatusCode(200, vehicleId);
+
+        }
+        catch (SqlException ex)
+        {
+            logger.LogWarning(ex.Message);
+            return StatusCode(400, new StatusErrorInformation(GenericError));
+        }
+        catch (Exception ex)
+        {
+            logger.LogInformation(ex.Message);
+            return StatusCode(403, new StatusErrorInformation(InvalidData));
+        }
+    }
+
+    [HttpPost]
+    [Route("/VehicleImageUpload")]
+    [Produces("application/json")]
+    public async Task<IActionResult> UploadVehicleImage([FromHeader] string vehicleId,[FromForm] IFormFile file)
+    {
+        if(file.Length == 0 || file == null)
+        {
+            return StatusCode(400, new StatusErrorInformation(GenericError));
+        }
+
+        string imageId = await imageService.UploadVehicleImage(file);
+
+        await  vehicleService.AddImageToVehicle(vehicleId, imageId);
+        return StatusCode(200);
+
+    }
+
 }
