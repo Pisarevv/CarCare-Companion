0b4c0f063241d6a9bc693f14d1faecc29cd02856
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-03 13:03:59 +0300

    Added image file validations

--- Client/CarCare-Companion/src/components/AddVehicle/AddVehicle.jsx
+++ Client/CarCare-Companion/src/components/AddVehicle/AddVehicle.jsx
@@ -55,13 +55,14 @@ const ValidationErrors = {
   emptyInput: "This field cannot be empty",
   inputNotNumber: "This field accepts only valid numbers",
   yearNotValid: `The start year has to be past 1900 and before ${new Date().getFullYear()}`,
-  invalidFileFormat : "The file format is not supported"
+  invalidFileFormat: "The file format is not supported",
+  fileSizeTooBig : "Please select a file under 2 MB"
 }
 
 const ValidationRegexes = {
   //Validates that the year is an integer 
   yearRegex: new RegExp(/^[0-9]*$/),
-  mileageRegex : new RegExp(/^[0-9]*$/)
+  mileageRegex: new RegExp(/^[0-9]*$/)
 }
 
 
@@ -74,8 +75,8 @@ const AddVehicle = (props) => {
   const { setLoading } = props;
 
   const [vehicleImage, setVehicleImage] = useState(null);
-  const [vehicleImageError,setVehicleImageError] = useState("");
-  const [isImageValid,setIsImageValid] = useState("false");
+  const [vehicleImageError, setVehicleImageError] = useState("");
+  const [isImageValid, setIsImageValid] = useState("false");
 
   const [vehicleId, setVehicleId] = useState("");
 
@@ -106,8 +107,8 @@ const AddVehicle = (props) => {
         var vehicleTypesResult = await getVehicleTypes();
         setFuelTypes(fuelTypes => fuelTypesResult);
         setVehicleTypes(vehicleTypes => vehicleTypesResult);
-        dispatch({ type: `SET_FUELTYPE`, payload: fuelTypesResult[0].id})
-        dispatch({ type: `SET_TYPE`, payload: vehicleTypesResult[0].id})
+        dispatch({ type: `SET_FUELTYPE`, payload: fuelTypesResult[0].id })
+        dispatch({ type: `SET_TYPE`, payload: vehicleTypesResult[0].id })
         setLoading(false);
       }
       catch (error) {
@@ -130,12 +131,17 @@ const AddVehicle = (props) => {
     };
 
     if (file) {
-      if(!validateImageFile(file)){
+      if (!validateImageFile(file) || !validateImageFileSize(file)) {
         setVehicleImage(vehicleImage => null);
+        return;
       }
       reader.readAsDataURL(file);
     }
 
+  }
+
+  const previousViewHandler = () => {
+    setStepOneFinished(stepOneFinished => false);
   }
 
   const onInputChange = (e) => {
@@ -162,22 +168,22 @@ const AddVehicle = (props) => {
   const validateNumberFields = (target, value) => {
     if (target === "mileage") {
       if (!ValidationRegexes.yearRegex.test(value) || value.trim() === "") {
-        dispatch({ type: `SET_${target.toUpperCase()}_ERROR`, payload: ValidationErrors.emptyInput});
+        dispatch({ type: `SET_${target.toUpperCase()}_ERROR`, payload: ValidationErrors.emptyInput });
         return false;
       }
       if (!ValidationRegexes.mileageRegex.test(value) || value.trim() === "") {
-        dispatch({ type: `SET_${target.toUpperCase()}_ERROR`, payload: ValidationErrors.inputNotNumber});
+        dispatch({ type: `SET_${target.toUpperCase()}_ERROR`, payload: ValidationErrors.inputNotNumber });
         return false;
       }
       return true;
     }
     if (target === "year") {
       if (!ValidationRegexes.yearRegex.test(value) || value.trim() === "") {
-        dispatch({ type: `SET_${target.toUpperCase()}_ERROR`, payload: ValidationErrors.emptyInput});
+        dispatch({ type: `SET_${target.toUpperCase()}_ERROR`, payload: ValidationErrors.emptyInput });
         return false;
       }
       if (Number(value) < 1900 || Number(value) > Number(new Date().getFullYear())) {
-        dispatch({ type: `SET_${target.toUpperCase()}_ERROR`, payload: ValidationErrors.yearNotValid});
+        dispatch({ type: `SET_${target.toUpperCase()}_ERROR`, payload: ValidationErrors.yearNotValid });
         return false;
       }
       return true;
@@ -193,8 +199,20 @@ const AddVehicle = (props) => {
     } else {
       setVehicleImageError(ValidationErrors.invalidFileFormat);
       setIsImageValid(false);
-      return false;   
+      return false;
     }
+  }
+
+  const validateImageFileSize = (file) => {
+    const fileSizeInMb = Math.round((file.size/1024));
+    if(fileSizeInMb < 2048){
+      setVehicleImageError(ValidationErrors.fileSizeTooBig);
+      setIsImageValid(true);
+      return true;
+    }
+      setVehicleImageError(ValidationErrors.fileSizeTooBig);
+      setIsImageValid(false);
+      return false;
   }
 
   const onVehicleAdd = async (e) => {
@@ -202,7 +220,7 @@ const AddVehicle = (props) => {
     try {
       let isMakeValid = validateTextFields("make", userVehicle.make);
       let isModelValid = validateTextFields("model", userVehicle.model);
-      let isFuelTypeValid = validateSelectFields("fuelType", userVehicle.fuelType); 
+      let isFuelTypeValid = validateSelectFields("fuelType", userVehicle.fuelType);
       let isMileageValid = validateNumberFields("mileage", userVehicle.mileage);
       let isTypeValid = validateSelectFields("type", userVehicle.type);
       let isYearValid = validateNumberFields("year", userVehicle.year);
@@ -229,7 +247,7 @@ const AddVehicle = (props) => {
 
   const onImageAdd = async (e) => {
     e.preventDefault();
-    if (vehicleImage) {
+    if (vehicleImage && isImageValid) {
       try {
         const formData = new FormData();
         formData.append("file", dataURLtoFile(vehicleImage, "inputImage"));
@@ -297,7 +315,7 @@ const AddVehicle = (props) => {
                   </form>
                 </>
                 :
-                <>
+                <div className="step-two-container">
                   <h2>Vehicle image</h2>
                   <form onSubmit={onImageAdd}>
                     <div className="input-group input-group-lg img-group">
@@ -308,11 +326,11 @@ const AddVehicle = (props) => {
                       {vehicleImageError && <p className="invalid-field">{vehicleImageError}</p>}
                     </div>
                     <div className="action-controls">
-                    <NavLink className="float" to={`/MyVehicles`}>Cancel</NavLink>
-                    <button type="submit" className="float">{vehicleImage ? "Finish" : "Skip and finish"}</button>
+                      <button className="float" onClick={previousViewHandler}>Previous</button>
+                      <button type="submit" className="float">{vehicleImage ? "Finish" : "Skip and finish"}</button>
                     </div>
                   </form>
-                </>
+                </div>
             }
           </div>
         </div>
