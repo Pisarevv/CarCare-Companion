4051b86f5d3853af2bad12aad8a85c0095e6ee10
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-04 21:46:45 +0300

    Add: Default vehicle logo
    The default logo will show up when a user hasn't added an image to his car

--- Client/CarCare-Companion/src/components/Vehicles/VehicleCard.jsx
+++ Client/CarCare-Companion/src/components/Vehicles/VehicleCard.jsx
@@ -6,7 +6,10 @@ const VehicleCard = ({vehicleData}) => {
     const { model, make, imageUrl, id } = vehicleData;
     return (
         <div className="vehicle-card">
-            <img src={imageUrl} className="vehicle-image"></img>
+            {imageUrl 
+            ? <img src={imageUrl} className="vehicle-image"></img> 
+            : <img src='../../public/car-logo.png' className="vehicle-image"></img> 
+            }
             <div className="vehicle-card-container">
                 <div className="vehicle-make">{make}</div>
                 <div className="vehicle-model">{model}</div>
