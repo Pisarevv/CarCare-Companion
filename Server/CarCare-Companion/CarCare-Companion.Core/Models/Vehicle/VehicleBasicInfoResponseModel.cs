a1c78d7eef5bb62341da8c1a4947a43f1ed00163
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-04 14:50:39 +0300

    Created model: VehicleBasicInfoResponseModel
    The model will serve as a basic vehicle details model.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Vehicle/VehicleBasicInfoResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CarCare_Companion.Core.Models.Vehicle;
+
+public class VehicleBasicInfoResponseModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Make { get; set; } = null!;
+
+    public string Model { get; set; } = null!;
+
+    public string? ImageUrl { get; set; }
+}
