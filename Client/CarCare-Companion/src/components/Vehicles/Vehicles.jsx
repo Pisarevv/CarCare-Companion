6e6f839f6a4e92a92ec721e2cae6ac6b4e214144
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-04 22:33:45 +0300

    Minor CSS changes and component add

--- Client/CarCare-Companion/src/components/Vehicles/Vehicles.jsx
+++ Client/CarCare-Companion/src/components/Vehicles/Vehicles.jsx
@@ -4,6 +4,7 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { getUserVehicles } from '../../services/vehicleService';
 
 import VehicleCard from './VehicleCard';
+import RecentTrips from './RecentTrips';
 
 import { ErrorHandler } from "../../utils/ErrorHandler/ErrorHandler";
 
@@ -37,7 +38,7 @@ const Vehicles = (props) => {
         <section className="vehicles">
             <div className="vehicle-container">
                 <div className="create-button"> <NavLink to="/Vehicle/Create">Add vehicle</NavLink></div>
-                <div className="div2"> test2</div>
+                <div className="user-recent-trips"><RecentTrips/></div>
                 <div className="div3"> test3</div>
                 <div className="div4"> test4</div>
                 <div className="user-vehicles"> {userVehicles.map(uv => <VehicleCard key ={uv.id} vehicleData = {uv}/>)}</div>
