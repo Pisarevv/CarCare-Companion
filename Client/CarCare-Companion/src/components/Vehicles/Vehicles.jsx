27e7184643533324d408bae5a18e43ef047cba88
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-04 14:49:35 +0300

    Edit: Vehicle component
    Added a variable to store the user vehicles.
    Minor changes in css.

--- Client/CarCare-Companion/src/components/Vehicles/Vehicles.jsx
+++ Client/CarCare-Companion/src/components/Vehicles/Vehicles.jsx
@@ -1,9 +1,25 @@
 import { NavLink, useNavigate } from 'react-router-dom';
+
 import './Vehicles.css';
+import { useEffect, useState } from 'react';
+import { getUserVehicles } from '../../services/vehicleService';
+import VehicleCard from './VehicleCard';
 
 const Vehicles = () => {
 
     const navigate = useNavigate();
+
+    const [userVehicles, setUserVehicles] = useState([]);
+
+
+    useEffect(() => {
+        (async() => {
+           const vehicles = await getUserVehicles();
+           setUserVehicles(userVehicles => vehicles);
+           console.log(vehicles);
+        })()
+    },[])
+
 
     return (
         <section className="vehicles">
@@ -12,7 +28,7 @@ const Vehicles = () => {
                 <div className="div2"> test2</div>
                 <div className="div3"> test3</div>
                 <div className="div4"> test4</div>
-                <div className="div5"> test5</div>
+                <div className="user-vehicles"> {userVehicles.map(uv => <VehicleCard key ={uv.id} vehicleData = {uv}/>)}</div>
             </div>
         </section>
     );
