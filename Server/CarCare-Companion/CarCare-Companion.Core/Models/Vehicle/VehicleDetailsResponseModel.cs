a00c62727a38e8eeb14f015f55110c548b667335
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-05 22:44:46 +0300

    Created response mode: VehicleDetailsResponseModel
    This model will be used when the user wants to access all the details about his vehicle.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Vehicle/VehicleDetailsResponseModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CarCare_Companion.Core.Models.Vehicle;
+
+public class VehicleDetailsResponseModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Make { get; set; } = null!;
+
+    public string Model { get; set; } = null!;
+
+    public string Mileage { get; set; } = null!;
+
+    public string? ImageUrl { get; set; } 
+
+    public string FuelType { get; set; } = null!;
+
+    public string VehicleType { get; set; } = null!;
+
+}
