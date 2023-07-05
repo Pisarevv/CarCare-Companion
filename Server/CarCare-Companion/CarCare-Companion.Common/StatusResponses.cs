7d8fc191bc8324cc8a5b610cf52cc352084b3c41
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-05 22:48:23 +0300

    Update StatusResponses.cs

--- Server/CarCare-Companion/CarCare-Companion.Common/StatusResponses.cs
+++ Server/CarCare-Companion/CarCare-Companion.Common/StatusResponses.cs
@@ -2,6 +2,7 @@
 public static class StatusResponses
 {
     public const string GenericError = "An error occurred while processing your request. Please try again later.";
+    public const string NoPermission = "No permission";
     public const string InvalidPassword = "Invalid password";
     public const string InvalidUser = "User does not exist";
     public const string InvalidData = "Invalid data";
@@ -10,6 +11,7 @@ public static class StatusResponses
     public const string UserEmailAlreadyExists = "A user with the same email already exists";
     public const string BadRequest = "Bad request";
     public const string FileSizeTooBig = "The size of the file is too big";
+    public const string ResourceNotFound = "The resource does not exist";
     public const string Success = "Success";
 
 
