025023c2fa7c3eabaccb8cb8aaa31af945047b1b
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-06 23:04:11 +0300

    Add: TripCreateRequestModel
    The model will be used on trip creating and editing

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Trip/TripCreateRequestModel.cs
@@ -0,0 +1,31 @@
+﻿namespace CarCare_Companion.Core.Models.Trip;
+
+using System.ComponentModel.DataAnnotations;
+
+using static CarCare_Companion.Common.ValidationConstants.TripRecord;
+
+public class TripCreateRequestModel
+{
+    [Required]
+    [MinLength(MinStartDestinationNameLength)]
+    [MaxLength(MaxStartDestinationNameLength)]
+    public string StartDestination { get; set; } = null!;
+
+    [Required]
+    [MinLength(MinEndDestinationNameLength)]
+    [MaxLength(MaxEndDestinationNameLength)]
+    public string EndDestination { get; set; } = null!;
+
+    [Required]
+    [Range(MinTravelledRange, MaxTravelledRange)]
+    public double MileageTravelled { get; set; }
+
+    [Range(MinFuelPrice, MaxFuelPrice)]
+    public double? UsedFuel { get; set; }
+
+    [Range(MinUsedFuel,MaxUsedFuel)]
+    public decimal? FuelPrice { get; set; }
+
+    public string VehicleId { get; set; } = null!;
+
+}
