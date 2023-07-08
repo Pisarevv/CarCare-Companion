f27a9550d114f8a1fb16d076376e1a77b6efee21
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-08 19:45:58 +0300

    Edit: AddTrip
    Split the trip adding process in two steps - trip details and fuel details.
    The user can optionally add fuel details about the trip.

--- Client/CarCare-Companion/src/components/AddTrip/AddTrip.jsx
+++ Client/CarCare-Companion/src/components/AddTrip/AddTrip.jsx
@@ -12,7 +12,7 @@ import { createTrip } from '../../services/tripService'
 const ValidationErrors = {
     emptyInput: "This field cannot be empty",
     inputNotNumber: "This field accepts only valid numbers",
-    fuelFields : "Both fields related to fuel have to be listed."
+    fuelFields: "Both fields related to fuel have to be listed."
 }
 
 const ValidationRegexes = {
@@ -27,6 +27,8 @@ const AddTrip = (props) => {
     const { setLoading } = props;
 
     const [userVehicles, setUserVehicles] = useState([]);
+
+    const [stepOneFinished, setStepOneFinished] = useState(false);
 
 
     const [trip, dispatch] = useReducer(tripReducer, {
@@ -82,34 +84,54 @@ const AddTrip = (props) => {
         return true;
     }
 
+    const onStepOneFinished = (e) => {
+        e.preventDefault()
+        let isStartDestinationValid = validateTextFields("startDestination", trip.startDestination);
+        let isEndDestinationValid = validateTextFields("endDestination", trip.endDestination);
+        let isMileageValid = validateNumberFields("travelledMileage", trip.travelledMileage);
+        let isVehicleValid = validateTextFields("vehicle", trip.vehicle);
+
+        if (isStartDestinationValid && isEndDestinationValid &&
+            isMileageValid && isVehicleValid) {
+            setStepOneFinished(stepOneFinished => true)
+        }
+        else {
+            setStepOneFinished(stepOneFinished => false)
+        }
+
+    }
+
+    const previousViewHandler = () => {
+        setStepOneFinished(stepOneFinished => false);
+    }
 
 
     const onTripAdd = async (e) => {
         e.preventDefault();
         try {
-            let isStartDestinationValid = validateTextFields("startDestination",trip.startDestination);
-            let isEndDestinationValid = validateTextFields("endDestination",trip.endDestination);
-            let isMileageValid = validateNumberFields("travelledMileage",trip.travelledMileage);
-            let isVehicleValid = validateTextFields("vehicle",trip.vehicle);
+            let isStartDestinationValid = validateTextFields("startDestination", trip.startDestination);
+            let isEndDestinationValid = validateTextFields("endDestination", trip.endDestination);
+            let isMileageValid = validateNumberFields("travelledMileage", trip.travelledMileage);
+            let isVehicleValid = validateTextFields("vehicle", trip.vehicle);
             if (!trip.usedFuel || !trip.fuelPrice) {
                 if (isStartDestinationValid && isEndDestinationValid &&
-                    isMileageValid && isVehicleValid) 
-                {
-                    const {startDestination,endDestination,travelledMileage,usedFuel,fuelPrice,vehicle} = trip;
-                    await createTrip(startDestination,endDestination,travelledMileage,usedFuel,fuelPrice,vehicle);
+                    isMileageValid && isVehicleValid) {
+                    const { startDestination, endDestination, travelledMileage, usedFuel, fuelPrice, vehicle } = trip;
+                    await createTrip(startDestination, endDestination, travelledMileage, null, null, vehicle);
                 }
             }
             else {
-                let isFuelPriceValid = validateNumberFields("fuelPrice",trip.fuelPrice);
-                let isUsedFuelValid = validateNumberFields("usedFuel",trip.usedFuel);
+                let isFuelPriceValid = validateNumberFields("fuelPrice", trip.fuelPrice);
+                let isUsedFuelValid = validateNumberFields("usedFuel", trip.usedFuel);
                 if (isStartDestinationValid && isEndDestinationValid &&
                     isMileageValid && isFuelPriceValid &&
-                    isUsedFuelValid && isVehicleValid)
-                {
-                    const {startDestination,endDestination,travelledMileage,usedFuel,fuelPrice,vehicle} = trip;
-                    await createTrip(startDestination,endDestination,travelledMileage,usedFuel,fuelPrice,vehicle);
+                    isUsedFuelValid && isVehicleValid) {
+                    const { startDestination, endDestination, travelledMileage, usedFuel, fuelPrice, vehicle } = trip;
+                    await createTrip(startDestination, endDestination, travelledMileage, usedFuel, fuelPrice, vehicle);
                 }
             }
+
+            navigate("/Trips")
 
         }
         catch (error) {
@@ -119,52 +141,71 @@ const AddTrip = (props) => {
     }
 
     return (
-        <section className="add-trip">
+        <section className="add-trip-section">
             <div className="add-trip-container">
                 <div className="add-trip-body">
-                    <div className="add-trip-heading">
-                        <h2>Trip details</h2>
-                        <form onSubmit={onTripAdd}>
-                            <div className="input-group input-group-lg">
-                                <label>Start destination:</label>
-                                <input className="form-control" type="text" placeholder="Start destination" name="startDestination" value={trip.startDestination} onChange={onInputChange} />
-                                {trip.startDestinationError && <p className="invalid-field" >{trip.startDestinationError}</p>}
-                            </div>
-                            <div className="input-group input-group-lg">
-                                <label>End destination:</label>
-                                <input className="form-control" type="text" placeholder="End destination" name="endDestination" value={trip.endDestination} onChange={onInputChange} />
-                                {trip.endDestinationError && <p className="invalid-field">{trip.endDestinationError}</p>}
-                            </div>
-                            <div className="input-group input-group-lg">
-                                <label>Travelled mileage:</label>
-                                <input className="form-control" type="text" placeholder="Travelled mileage" name="travelledMileage" value={trip.travelledMileage} onChange={onInputChange} />
-                                {trip.travelledMileage && <p className="invalid-field" >{trip.travelledMileageError}</p>}
-                            </div>
-                            <div className="input-group input-group-lg">
-                                <label>Used fuel:</label>
-                                <input className="form-control" type="text" placeholder="Used fuel" name="usedFuel" value={trip.usedFuel} onChange={onInputChange} />
-                                {trip.usedFuelError && <p className="invalid-field" >{trip.usedFuelError}</p>}
-                                {(trip.usedFuelError && !trip.fuelPrice ) && <p className="invalid-field" >{ValidationErrors.fuelFields}</p>}
-                            </div>
-                            <div className="input-group input-group-lg">
-                                <label>Fuel price:</label>
-                                <input className="form-control" type="text" placeholder="Fuel price" name="fuelPrice" value={trip.fuelPrice} onChange={onInputChange} />
-                                {trip.typeError && <p className="invalid-field">{trip.typeError}</p>}
-                                {(!trip.usedFuel && trip.fuelPrice) && <p className="invalid-field" >{ValidationErrors.fuelFields}</p>}
-                            </div>
-                            <div className="input-group input-group-lg">
-                                <label>Vehicle:</label>
-                                <div className="form-control select">
-                                    <select className="select-group" name="vehicle" onChange={onInputChange}>
-                                        {userVehicles.map(uv => <option key={uv.id} value={uv.id}>{`${uv.make} ${uv.model}`}</option>)}
-                                    </select>
+
+                    {
+                        stepOneFinished == false
+                            ?
+                            <>
+                                <div className="add-trip-heading">
+                                    <h2>Trip details</h2>
                                 </div>
-                                {trip.vehicle && <p className="invalid-field" >{trip.vehicleError}</p>}
-                            </div>
-                            <NavLink className="float" to={`/Trips`}>Cancel</NavLink>
-                            <button type="submit" className="float">Add trip</button>
-                        </form>
-                    </div>
+                                <form onSubmit={onStepOneFinished}>
+                                    <div className="input-group input-group-lg">
+                                        <label>Start destination:</label>
+                                        <input className="form-control" type="text" placeholder="Start destination" name="startDestination" value={trip.startDestination} onChange={onInputChange} />
+                                        {trip.startDestinationError && <p className="invalid-field" >{trip.startDestinationError}</p>}
+                                    </div>
+                                    <div className="input-group input-group-lg">
+                                        <label>End destination:</label>
+                                        <input className="form-control" type="text" placeholder="End destination" name="endDestination" value={trip.endDestination} onChange={onInputChange} />
+                                        {trip.endDestinationError && <p className="invalid-field">{trip.endDestinationError}</p>}
+                                    </div>
+                                    <div className="input-group input-group-lg">
+                                        <label>Travelled mileage:</label>
+                                        <input className="form-control" type="text" placeholder="Travelled mileage" name="travelledMileage" value={trip.travelledMileage} onChange={onInputChange} />
+                                        {trip.travelledMileageError && <p className="invalid-field" >{trip.travelledMileageError}</p>}
+                                    </div>
+                                    <div className="input-group input-group-lg">
+                                        <label>Vehicle:</label>
+                                        <div className="form-control select">
+                                            <select className="select-group" name="vehicle" onChange={onInputChange}>
+                                                {userVehicles.map(uv => <option key={uv.id} value={uv.id}>{`${uv.make} ${uv.model}`}</option>)}
+                                            </select>
+                                        </div>
+                                        {trip.vehicle && <p className="invalid-field" >{trip.vehicleError}</p>}
+                                    </div>
+                                    <NavLink className="float" to={`/Trips`}>Cancel</NavLink>
+                                    <button type="submit" className="float">Next step</button>
+                                </form>
+                            </>
+                            :
+                            <>
+                                <div className="add-trip-heading">
+                                    <h2>Fuel details (Optional)</h2>
+                                </div>
+                                <form onSubmit={onTripAdd}>
+                                    <div className="input-group input-group-lg">
+                                        <label>Used fuel:</label>
+                                        <input className="form-control" type="text" placeholder="Used fuel" name="usedFuel" value={trip.usedFuel} onChange={onInputChange} />
+                                        {trip.usedFuelError && <p className="invalid-field" >{trip.usedFuelError}</p>}
+                                        {(trip.usedFuel && !trip.fuelPrice) && <p className="warning-field" >{ValidationErrors.fuelFields}</p>}
+                                    </div>
+                                    <div className="input-group input-group-lg">
+                                        <label>Fuel price per liter:</label>
+                                        <input className="form-control" type="text" placeholder="Fuel price" name="fuelPrice" value={trip.fuelPrice} onChange={onInputChange} />
+                                        {trip.typeError && <p className="invalid-field">{trip.typeError}</p>}
+                                        {(!trip.usedFuel && trip.fuelPrice) && <p className="warning-field" >{ValidationErrors.fuelFields}</p>}
+                                    </div>
+
+                                    <button className="float" onClick={previousViewHandler}>Previous</button>
+                                    <button type="submit" className="float">{trip.usedFuel && trip.fuelPrice ? "Finish" : "Skip and finish"}</button>
+                                </form>
+                            </>
+                    }
+
                 </div>
             </div>
         </section>
