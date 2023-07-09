a3bca7b222e8857eb2881c97299fb38c9df6becd
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-09 20:05:37 +0300

    Add: Trip details model
    The model contains details about a user trip

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Trip/TripDetailsByUserResponseModel.cs
@@ -0,0 +1,24 @@
+﻿namespace CarCare_Companion.Core.Models.Trip;
+
+public class TripDetailsByUserResponseModel
+{
+    public string Id { get; set; } = null!;
+
+    public string StartDestination { get; set; } = null!;
+
+    public string EndDestination { get; set; } = null!;
+
+    public double MileageTravelled { get; set; }
+
+    public double? UsedFuel { get; set; }
+
+    public decimal? FuelPrice { get; set; }
+
+    public decimal? TripCost { get; set; }
+
+    public string VehicleMake { get; set; } = null!;
+
+    public string VehicleModel { get; set; } = null!;
+
+    public DateTime DateCreated { get; set; }
+}
