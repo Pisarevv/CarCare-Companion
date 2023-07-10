91f5606b0b9fe051222f2a3ff580f85432e2057a
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-10 11:23:16 +0300

    Update: Endpoints update

--- Client/CarCare-Companion/src/services/vehicleService.jsx
+++ Client/CarCare-Companion/src/services/vehicleService.jsx
@@ -2,31 +2,31 @@ import * as api from './api';
 import * as fileApi from './fileApi';
 
 export async function getFuelTypes(){
-    var result = await api.get("/FuelTypes")
+    var result = await api.get("/Vehicles/FuelTypes")
     return result;
 }
 
 export async function getVehicleTypes(){
-    var result = await api.get("/VehicleTypes")
+    var result = await api.get("Vehicles/Types")
     return result;
 }
 
 export async function createVehicle(make,model,mileage,year,fuelTypeId, vehicleTypeId, ownerId){
-    var result = await api.post("/VehicleCreate",{make,model,mileage,year,fuelTypeId, vehicleTypeId, ownerId})
+    var result = await api.post("/Vehicles",{make,model,mileage,year,fuelTypeId, vehicleTypeId, ownerId})
     return result;
 }
 
 export async function uploadVehicleImage(formData,vehicleId){
-    var result = await fileApi.post("/VehicleImageUpload",formData,vehicleId);
+    var result = await fileApi.post("/Vehicles/ImageUpload",formData,vehicleId);
     return result;
 }
 
 export async function getUserVehicles(){
-    var result = await api.get("/UserVehicles");
+    var result = await api.get("/Vehicles");
     return result;
 }
 
 export async function getVehicleDetails(vehicleId){
-    var result = await api.get(`/VehicleDetails/${vehicleId}`);
+    var result = await api.get(`/Vehicles/Details/${vehicleId}`);
     return result;
 }
