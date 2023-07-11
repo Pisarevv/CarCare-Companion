04f9d68401506e12a4f9d00cb5e2a0f6c69fda54
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-11 16:17:37 +0300

    Remove: Unnecessary method

--- Server/CarCare-Companion/CarCare-Companion.Core/Contracts/IIdentityService.cs
+++ Server/CarCare-Companion/CarCare-Companion.Core/Contracts/IIdentityService.cs
@@ -6,8 +6,6 @@ public interface IIdentityService
 {
     public Task<bool> DoesUserExistByUsernameAsync(string username);
 
-    public Task<bool> DoesUserExistByIdAsync(string userId);
-
     public Task<AuthDataModel> RegisterAsync(RegisterRequestModel model);
 
     public Task<AuthDataModel> LoginAsync(LoginRequestModel model);
