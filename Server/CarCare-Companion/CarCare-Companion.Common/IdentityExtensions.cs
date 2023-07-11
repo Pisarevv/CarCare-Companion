e4b80d8b9724a05613d0827b13245c5b8c469afd
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-11 16:15:05 +0300

    Add: IdentityExtensions
    The added extension class will provide different extensions methods based on the identity user

---
+++ Server/CarCare-Companion/CarCare-Companion.Common/IdentityExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace CarCare_Companion.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class IdentityExtensions
+{
+}
