2c27e73824241692960ffe6bd593d683b63599ad
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-12 12:11:39 +0300

    Add: VehicleDelete component
    The page will be  used by the user to confirm that he wants to delete the selected vehicle.

---
+++ Client/CarCare-Companion/src/components/VehicleDetails/VehicleDelete.jsx
@@ -0,0 +1,45 @@
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
+import './VehicleDelete.css'
+import { deleteVehicle } from '../../services/vehicleService';
+import { ErrorHandler } from '../../utils/ErrorHandler/ErrorHandler';
+
+const VehicleDelete = () => {
+    const location = useLocation();
+    const { id } = useParams();
+
+    const details = location.state.details;
+    const { make, model } = details;
+
+    const navigate = useNavigate();
+
+   
+
+    const onClickHander = async (e) => {
+      e.preventDefault();
+      try {
+        await deleteVehicle(id);
+        navigate("/MyVehicles")
+      } 
+      catch (error) {
+        ErrorHandler(error)
+        navigate(`/Vehicle/Details/${id}`)
+      }
+    }
+
+    return (
+        <section className="vehicle-delete-section" >
+            <div className="vehicle-delete-card">
+                <div className="vehicle-delete-card-container">
+                    <div>Are you sure you want to delete your {make} {model}?</div>
+                    <div>All the trip,service and tax records related to the vehicle will be deleted.</div>
+                </div>
+            </div>
+            <div className="vehicle-delete-buttons">
+                <Link className="float" to={`/Vehicle/Details/${id}`}>Cancel</Link>
+                <button className="float" onClick={onClickHander}>Confirm</button>
+            </div>
+        </section>
+    )
+}
+
+export default VehicleDelete;
