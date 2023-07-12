b8af995f8bacc9940754443ab162d048fdae2d67
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-12 15:30:45 +0300

    Create : TripBasicInformationByUserResponseModel
    This model is used to transfer data for the most recent user trips

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Trip/TripBasicInformationByUserResponseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CarCare_Companion.Core.Models.Trip;
+
+public class TripBasicInformationByUserResponseModel
+{
+    public string Id { get; set; } = null!;
+
+    public string StartDestination { get; set; } = null!;
+
+    public string EndDestination { get; set; } = null!;
+
+    public double MileageTravelled { get; set; }
+
+    public string Vehicle { get; set; } = null!;
+}
