5578ba366cbbd7df5da3a530dd6bd0a6f1fe411c
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-13 23:30:03 +0300

    Add: VehicleDetailsEditResponseModel
    This model will transfer data when the user want to edit the vehicle details.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Vehicle/VehicleDetailsEditResponseModel.cs
@@ -0,0 +1,21 @@
+﻿namespace CarCare_Companion.Core.Models.Vehicle;
+
+public class VehicleDetailsEditResponseModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Make { get; set; } = null!;
+
+    public string Model { get; set; } = null!;
+
+    public double Mileage { get; set; }
+
+    public int Year { get; set; }
+
+    public string? ImageUrl { get; set; } 
+
+    public int FuelTypeId { get; set; }
+
+    public int VehicleTypeId { get; set;}
+
+}
