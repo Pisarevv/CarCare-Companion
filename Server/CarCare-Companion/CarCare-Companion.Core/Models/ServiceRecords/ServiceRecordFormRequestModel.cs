f1fcfda646a7dc75b0cd7e204fccdb9302e15632
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-16 22:52:10 +0300

    Add: new request model
    Created a new form request model - ServiceRecordFormRequestModel. This model will be used for the creation and edit of a user service record.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/ServiceRecords/ServiceRecordFormRequestModel.cs
@@ -0,0 +1,33 @@
+﻿namespace CarCare_Companion.Core.Models.ServiceRecords;
+
+using Microsoft.EntityFrameworkCore;
+
+using System.ComponentModel.DataAnnotations;
+
+using static Common.ValidationConstants.ServiceRecord;
+
+public class ServiceRecordFormRequestModel
+{
+    [Required]
+    [MinLength(MinTitleLength)]
+    [MaxLength(MaxTitleLength)]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    public string PerformedOn { get; set; } = null!;
+
+    [Required]
+    [Range(MinMileage, MaxMileage)]
+    public double Mileage { get; set; }
+
+    public string? Description { get; set; }
+
+    [Required]
+    [Range(MinCost, MaxCost)]
+    [Precision(18, 2)]
+    public decimal Cost { get; set; }
+
+    [Required]
+    public string VehicleId { get; set; } = null!;
+
+}
