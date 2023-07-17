015113d4b34b824455c67022fbd647e0cd38e962
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-17 12:22:37 +0300

    Add: new response model
    Created a new response model - TripEditDetailsResponseModel. This model will be used to provide the trip record details to the user trip edit component.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/TripRecords/TripEditDetailsResponseModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CarCare_Companion.Core.Models.Trip;
+
+public class TripEditDetailsResponseModel
+{
+    public string Id { get; set; } = null!;
+
+    public string StartDestination { get; set; } = null!;
+
+    public string EndDestination { get; set; } = null!;
+
+    public double MileageTravelled { get; set; }
+
+    public double? UsedFuel { get; set; }
+
+    public decimal? FuelPrice { get; set; }
+
+    public string Vehicle{ get; set; } = null!;
+}
