40e5f5df11d6d872b3a17b23ac967cf1a0cb1950
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-17 14:20:13 +0300

    Add: Edit and Delete buttons
    Added edit and delete buttons with routes for reaching the dedicated components.

--- Client/CarCare-Companion/src/components/Trips/UserTripCard.jsx
+++ Client/CarCare-Companion/src/components/Trips/UserTripCard.jsx
@@ -1,19 +1,28 @@
+import { Link } from 'react-router-dom';
 import './UserTripCard.css'
 
-const UserTripCard = ({tripDetails}) => {
-    
-    const {dateCreated, startDestination,endDestination,mileageTravelled,fuelPrice, usedFuel, tripCost, vehicleMake, vehicleModel} = tripDetails;
+const UserTripCard = ({ tripDetails }) => {
+
+    const {id, dateCreated, startDestination, endDestination, mileageTravelled, fuelPrice, usedFuel, tripCost, vehicleMake, vehicleModel } = tripDetails;
     return (
-        
+
         <div className="user-trip-card-container">
             <div className="trip-card-border"></div>
             <div>Trip on: {dateCreated}. From {startDestination} to {endDestination}.</div>
             <div>Distance: {mileageTravelled} km</div>
-            {usedFuel &&  <div>Used fuel (liters): {usedFuel} levs.</div>}  
+            {usedFuel && <div>Used fuel (liters): {usedFuel} liters.</div>}
             {fuelPrice && <div>Fuel price during trip: {fuelPrice} levs.</div>}
             {tripCost && <div>Trip cost: {tripCost} levs.</div>}
             <div>Vehicle: {vehicleMake} {vehicleModel}</div>
             <div className="trip-card-border"></div>
+            <div className='trip-actions-container'>
+                <Link to={`/Trips/Edit/${id}`}>Edit</Link>
+                <Link
+                    to={`/Trip/Delete/${id}`}
+                    state={{ details: { startDestination, endDestination, vehicleMake, vehicleModel } }}
+                >Delete
+                </Link>
+            </div>
         </div>
 
     )
