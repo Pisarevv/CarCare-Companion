549ae9eee2b1d601a08d617d5ad1969dcec902d6
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-17 15:24:18 +0300

    Add: new component DeleteTrip
    Created a new functional component, DeleteTrip, to display a validation form that the user is sure about the delete actions.

---
+++ Client/CarCare-Companion/src/components/DeleteTrip/DeleteTrip.jsx
@@ -0,0 +1,43 @@
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
+import './DeleteTrip.css'
+import { deleteTrip } from '../../services/tripService';
+import { NotificationHandler } from '../../utils/NotificationHandler'
+
+const DeleteTrip = () => {
+
+    const location = useLocation();
+    const { id } = useParams();
+
+    const details = location.state.details;
+    const { startDestination, endDestination, vehicleMake, vehicleModel } = details;
+
+    const navigate = useNavigate();
+
+
+    const onClickHander = async (e) => {
+      e.preventDefault();
+      try {
+        await deleteTrip(id); 
+      } 
+      catch (error) {
+        NotificationHandler(error)
+      }
+      navigate("/Trips")
+    }
+
+    return (
+        <section className="trip-delete-section" >
+            <div className="trip-delete-card">
+                <div className="trip-delete-card-container">
+                    <div>Are you sure you want to delete your trip from {startDestination} to {endDestination} with {vehicleMake} {vehicleModel}?</div>
+                </div>
+            </div>
+            <div className="trip-delete-buttons">
+                <Link className="float" to={`/Trips`}>Cancel</Link>
+                <button className="float" onClick={onClickHander}>Confirm</button>
+            </div>
+        </section>
+    )
+}
+
+export default DeleteTrip;
