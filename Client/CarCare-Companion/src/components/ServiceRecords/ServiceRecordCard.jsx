debd60f1b8c491190f6a1b4b99510c3f90027a82
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-18 11:00:10 +0300

    Add: new component ServiceRecordCard
    Created a new functional component, ServiceRecordCard. This component is responsible for rendering the service records retrieved from parent component and display the service record.

---
+++ Client/CarCare-Companion/src/components/ServiceRecords/ServiceRecordCard.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+import './ServiceRecordCard.css'
+
+const ServiceRecordCard = ({ serviceRecordDetails }) => {
+
+    const {id, title, performedOn, mileage, description, cost, vehicleMake, vehicleModel } = serviceRecordDetails;
+    return (
+
+        <div className="service-record-card-container">
+            <div className="service-record-card-border"></div>
+            <div>Service {title} performed on {performedOn}.</div>
+            <div>Vehicle: {vehicleMake} {vehicleModel}.</div>
+            <div>Mileage: {mileage} km.</div>
+            <div>Cost: {cost} km.</div>
+            {description && <div>Description: {description}</div>}
+            <div className="service-record-card-border"></div>
+            <div className="service-record-actions-container">
+                <Link to={`/ServiceRecords/Edit/${id}`}>Edit</Link>
+                <Link
+                    to={`/ServiceRecords/Delete/${id}`}
+                    state={{ details: { title, performedOn, vehicleMake, vehicleModel } }}
+                >Delete
+                </Link>
+            </div>
+        </div>
+
+    )
+}
+
+export default ServiceRecordCard;
