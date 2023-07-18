915db907523abff4c1a7d8c36c7c2f0a0a660364
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-18 22:49:07 +0300

    Add: Service records rendering

--- Client/CarCare-Companion/src/components/ServiceRecords/ServiceRecords.jsx
+++ Client/CarCare-Companion/src/components/ServiceRecords/ServiceRecords.jsx
@@ -1,24 +1,34 @@
-import './ServiceRecords.css'
-
-import IsLoadingHOC from '../Common/IsLoadingHoc';
 import { useEffect, useState } from 'react';
-import ServiceRecordCard from './ServiceRecordCard';
+
+import { Link } from 'react-router-dom';
+
 import { NotificationHandler } from '../../utils/NotificationHandler';
 
+import { getAllServiceRecords } from '../../services/serviceRecordsService';
+
+import IsLoadingHOC from '../Common/IsLoadingHoc';
+
+import ServiceRecordCard from './ServiceRecordCard';
+
+import './ServiceRecords.css'
+
 const ServiceRecords = (props) => {
- 
+
     const { setLoading } = props;
 
     const [serviceRecords, setServiceRecords] = useState([]);
 
     useEffect(() => {
-        (async() => {
+        (async () => {
             try {
+                let serviceRecordsResult = await getAllServiceRecords();
+                console.log(serviceRecordsResult);
+                setServiceRecords(serviceRecords => serviceRecordsResult);
                 setLoading(false);
-            } 
+            }
             catch (error) {
-              NotificationHandler(error);
-              setLoading(false);   
+                NotificationHandler(error);
+                setLoading(false);
             }
         })()
     }, [])
@@ -26,14 +36,16 @@ const ServiceRecords = (props) => {
     return (
         <section className='service-records-section'>
             <div className='service-records-container'>
-                <div className="services-statistics"> </div>
+                <div className="services-statistics">
+                    <Link id = "add-record" to="/ServiceRecords/Add">Add service record</Link>
+                </div>
                 <div className="service-records-list">
-                    { 
-                    serviceRecords.length > 0 
-                    ? 
-                    serviceRecords.map(sr => <ServiceRecordCard key = {sr.Id} serviceRecordDetails = {sr}/>)
-                    :
-                    <div>You haven't added any service records yet.</div>
+                    {
+                        serviceRecords.length > 0
+                            ?
+                            serviceRecords.map(sr => <ServiceRecordCard key={sr.id} serviceRecordDetails={sr} />)
+                            :
+                            <div>You haven't added any service records yet.</div>
                     }
                 </div>
             </div>
