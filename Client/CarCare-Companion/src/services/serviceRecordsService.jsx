19d7e1e164aba4f8c22c3c62e425ac5722eee27d
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-18 16:49:52 +0300

    Add: new service method
     createServiceRecord - this method is reposnsible for sending a request for creating a service record. It sends the title, description, cost, performed on date and vehicle id.
    On a successful creation it receives the created record Id

---
+++ Client/CarCare-Companion/src/services/serviceRecordsService.jsx
@@ -0,0 +1,16 @@
+/**
+ * serviceRecordsService 
+ * ---------------------
+ * The service responsible for the CRUD operations of the record services.
+ * ---------------------- 
+**/
+
+import * as api from "./api"
+
+
+export async function createServiceRecord(title, description, mileage, cost, vehicleId, performedOn){
+    var result = await api.post("/ServiceRecords", {title, description, mileage, cost, vehicleId, performedOn});
+    return result;
+}
+
+
