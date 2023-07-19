99e5ac55ef0c788039b8e5b63fb83d1fd7b36a30
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-19 10:34:03 +0300

    Edit: utility function - StringToISODateString
    Added additional refracturing to make the input string from "dd/MM/yyy" to an ISO 8601 format.

--- Client/CarCare-Companion/src/utils/StringToISODateString.jsx
+++ Client/CarCare-Companion/src/utils/StringToISODateString.jsx
@@ -1,15 +1,26 @@
 /**
  *  StringToISODateString
- *  This function that recieves a string in a format "MM/dd/yyyy" 
+ *  This function that recieves a string in a format "dd/MM/yyyy" 
  * and converts it to a ISO 8601 datetime string
  * -----------------
 **/
 
-const StringToISODateString = (inputString) => {
-    const dateTimeStamp = Date.parse(inputString);
 
+const StringToISODateString = (inputString) => {
+
+    //Changing all '/' with '-'
+    const formatedInput = inputString.replaceAll('/','-');
+
+    //Formatting the string in  yyyy/MM/dd format to get the correct time stamp
+    const formattedString = formatedInput.substring(6,10) + formatedInput.substring(2,6) + formatedInput.substring(0,2);
+
+    //Making the correct time stamp for date
+    const dateTimeStamp = Date.parse(formattedString);
+
+    //Creating a new date based on the time stamp
     const date = new Date(dateTimeStamp);
 
+    //Returning the date in an ISO 8601 format - example - 2023‐07‐19T06:01:31Z
     return date.toISOString();
     
 }
