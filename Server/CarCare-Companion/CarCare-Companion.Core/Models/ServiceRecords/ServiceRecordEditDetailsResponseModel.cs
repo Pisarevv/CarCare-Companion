e2f723717d1c4979bc9a73f0637203c8d86831c8
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-19 11:39:59 +0300

    Add : new response model
    ServiceRecordEditDetailsResponseModel - this model will transfer data when the user want to edit the service record details.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/ServiceRecords/ServiceRecordEditDetailsResponseModel.cs
@@ -0,0 +1,18 @@
+﻿namespace CarCare_Companion.Core.Models.ServiceRecords;
+
+public class ServiceRecordEditDetailsResponseModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public DateTime PerformedOn { get; set; }
+
+    public double Mileage { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal Cost { get; set; }
+
+    public string VehicleId { get; set; } = null!;
+}
