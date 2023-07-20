4ce4803a383ffc5d347b36437de7bb3392575607
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-20 14:40:22 +0300

    Add: new service method - CreateAsync
    The method is responsible for creating and saving a new tax record. The input parameters are the user id and tax record form model containing the information.

--- Server/CarCare-Companion/CarCare-Companion.Core/Contracts/ITaxRecordsService.cs
+++ Server/CarCare-Companion/CarCare-Companion.Core/Contracts/ITaxRecordsService.cs
@@ -1,8 +1,9 @@
 ﻿namespace CarCare_Companion.Core.Contracts;
 
+using CarCare_Companion.Core.Models.TaxRecords;
 
 public interface  ITaxRecordsService
 {
-
+    public Task<string> CreateAsync(string userId, TaxRecordFormRequestModel model);
 }
 
--- Server/CarCare-Companion/CarCare-Companion.Core/Services/TaxRecordsService.cs
+++ Server/CarCare-Companion/CarCare-Companion.Core/Services/TaxRecordsService.cs
@@ -1,8 +1,44 @@
 ﻿namespace CarCare_Companion.Core.Services;
 
+using System.Threading.Tasks;
+
 using CarCare_Companion.Core.Contracts;
+using CarCare_Companion.Core.Models.TaxRecords;
+using CarCare_Companion.Infrastructure.Data.Common;
+using CarCare_Companion.Infrastructure.Data.Models.Records;
 
 public class TaxRecordsService : ITaxRecordsService
 {
+    private readonly IRepository repository;
 
+    public TaxRecordsService(IRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    /// <summary>
+    /// Creates a new tax record
+    /// </summary>
+    /// <param name="model">The input model containing the tax record information</param>
+    /// <param name="userId">The user identifier</param>
+    /// <returns>String containing the newly created tax record Id</returns>
+    public async Task<string> CreateAsync(string userId, TaxRecordFormRequestModel model)
+    {
+        TaxRecord recordToAdd = new TaxRecord()
+        {
+            Title = model.Title,
+            ValidFrom = model.ValidFrom,
+            ValidTo = model.ValidTo,
+            Cost = model.Cost,
+            Description = model.Description,
+            VehicleId = Guid.Parse(model.VehicleId),
+            OwnerId = Guid.Parse(userId),
+            CreatedOn = DateTime.UtcNow
+        };
+
+        await repository.AddAsync(recordToAdd);
+        await repository.SaveChangesAsync();
+
+        return recordToAdd.Id.ToString();
+    }
 }
