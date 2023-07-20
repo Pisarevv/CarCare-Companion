339826196aade28eb798306bd801cd0ff727e471
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-20 14:43:38 +0300

    Add: new controller method - Create
    Implemented a new method, Create, in the TaxRecordsController class. This method handles request for trip record deleting by the user.
    
    The Create accepts the model containing the record information.
    
    Validates the input data to ensure it meets the required criteria:
    - Does the user exist
    - Does the vehicle exist
    - Is the user the vehicle owner
    If the validation steps are valid - the service method for tax record creating is called.

--- Server/CarCare-Companion/CarCare-Companion.Api/Controllers/TaxRecordsController.cs
+++ Server/CarCare-Companion/CarCare-Companion.Api/Controllers/TaxRecordsController.cs
@@ -1,8 +1,14 @@
 ﻿namespace CarCare_Companion.Api.Controllers;
 
-using CarCare_Companion.Core.Contracts;
-using CarCare_Companion.Core.Services;
 using Microsoft.AspNetCore.Mvc;
+
+using CarCare_Companion.Common;
+using CarCare_Companion.Core.Contracts;
+using CarCare_Companion.Core.Models.Status;
+using CarCare_Companion.Core.Models.TaxRecords;
+using CarCare_Companion.Core.Services;
+
+using static Common.StatusResponses;
 
 
 /// <summary>
@@ -20,5 +26,44 @@ public class TaxRecordsController : BaseController
         this.taxRecordsService = taxRecordsService;
         this.vehicleService = vehicleService;
         this.logger = logger;
+    }
+
+    /// <summary>
+    /// Creates a new tax record
+    /// </summary>
+    /// <param name="model">The model containing the tax record details</param>
+    /// <returns>The Id of the tax record trip</returns>
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] TaxRecordFormRequestModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return StatusCode(400, new StatusInformationMessage(InvalidData));
+        }
+        string? userId = this.User.GetId();
+
+        if(string.IsNullOrEmpty(userId) )
+        {
+            return StatusCode(403, new StatusInformationMessage(InvalidUser));
+        }
+
+        bool doesVehicleExist = await vehicleService.DoesVehicleExistByIdAsync(model.VehicleId);
+
+        if (!doesVehicleExist)
+        {
+            return StatusCode(404, ResourceNotFound);
+        }
+
+        bool isUserVehicleOwner = await vehicleService.IsUserOwnerOfVehicleAsync(userId, model.VehicleId);
+
+        if (!isUserVehicleOwner)
+        {
+            return StatusCode(400, new StatusInformationMessage(InvalidData));
+        }
+
+        string recordId = await taxRecordsService.CreateAsync(userId, model);
+
+        return StatusCode(201, recordId);
+
     }
 }
