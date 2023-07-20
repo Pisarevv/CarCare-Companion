07341bcd986f59f5e0046f30914ca4971903f473
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-20 11:21:26 +0300

    Add: new controller - TaxRecordsController
    Introduced a new controller, TaxRecordsController, to manage tax records-related operations within the application. This controller handles requests related to tax records and interacts with the dedicated services to retrieve and manipulate tax records data.

---
+++ Server/CarCare-Companion/CarCare-Companion.Api/Controllers/TaxRecordsController.cs
@@ -0,0 +1,24 @@
+﻿namespace CarCare_Companion.Api.Controllers;
+
+using CarCare_Companion.Core.Contracts;
+using CarCare_Companion.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+
+/// <summary>
+/// The service records controller handles tax records related operations
+/// </summary>
+[Route("[controller]")]
+public class TaxRecordsController : BaseController
+{
+    private readonly ITaxRecordsService taxRecordsService;
+    private readonly IVehicleService vehicleService;
+    private readonly ILogger<TaxRecordsService> logger;
+
+    public TaxRecordsController(ITaxRecordsService taxRecordsService, IVehicleService vehicleService, ILogger<TaxRecordsService> logger)
+    {
+        this.taxRecordsService = taxRecordsService;
+        this.vehicleService = vehicleService;
+        this.logger = logger;
+    }
+}
