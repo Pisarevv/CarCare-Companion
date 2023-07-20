1f9c32e8cbc656a0239c9cd99811d408a6ae9af7
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-20 15:36:24 +0300

    Add: new service method - GetAllByUserIdAsync
    The method retrieves all the user tax records ordered by the time of creation that are not deleted. The input parameter is the user Id.

--- Server/CarCare-Companion/CarCare-Companion.Core/Contracts/ITaxRecordsService.cs
+++ Server/CarCare-Companion/CarCare-Companion.Core/Contracts/ITaxRecordsService.cs
@@ -5,5 +5,7 @@ using CarCare_Companion.Core.Models.TaxRecords;
 public interface  ITaxRecordsService
 {
     public Task<string> CreateAsync(string userId, TaxRecordFormRequestModel model);
+
+    public Task<ICollection<TaxRecordResponseModel>> GetAllByUserIdAsync(string userId);
 }
 
--- Server/CarCare-Companion/CarCare-Companion.Core/Services/TaxRecordsService.cs
+++ Server/CarCare-Companion/CarCare-Companion.Core/Services/TaxRecordsService.cs
@@ -1,11 +1,15 @@
 ﻿namespace CarCare_Companion.Core.Services;
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
 
 using CarCare_Companion.Core.Contracts;
 using CarCare_Companion.Core.Models.TaxRecords;
 using CarCare_Companion.Infrastructure.Data.Common;
 using CarCare_Companion.Infrastructure.Data.Models.Records;
+
 
 public class TaxRecordsService : ITaxRecordsService
 {
@@ -14,6 +18,30 @@ public class TaxRecordsService : ITaxRecordsService
     public TaxRecordsService(IRepository repository)
     {
         this.repository = repository;
+    }
+
+    /// <summary>
+    /// Retrieves all user tax records
+    /// </summary>
+    /// <param name="userId">The user identifier</param>
+    /// <returns>Collection of tax records</returns>
+    public async Task<ICollection<TaxRecordResponseModel>> GetAllByUserIdAsync(string userId)
+    {
+        return await repository.AllReadonly<TaxRecord>()
+               .Where(tr => tr.IsDeleted == false)
+               .Where(tr => tr.OwnerId == Guid.Parse(userId))
+               .OrderBy(tr => tr.CreatedOn) 
+               .Select(tr => new TaxRecordResponseModel()
+               {
+                   Id = tr.Id.ToString(),
+                   Title = tr.Title,
+                   ValidFrom = tr.ValidFrom,
+                   ValidTo = tr.ValidTo,
+                   Cost = tr.Cost,
+                   VehicleMake = tr.Vehicle.Make,
+                   VehicleModel = tr.Vehicle.Model,
+               })
+               .ToListAsync();
     }
 
     /// <summary>
