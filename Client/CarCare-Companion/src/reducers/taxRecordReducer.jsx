acce837a1c678814406ee3b9ad06c81bd01c4615
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-20 10:31:17 +0300

    Add: new reducer - taxRecordReducer
    Implemented a new reducer that will be in charge of state handling for tax records creating and editing.

---
+++ Client/CarCare-Companion/src/reducers/taxRecordReducer.jsx
@@ -0,0 +1,40 @@
+/**
+ * taxRecordReducer  
+ * ---------------------
+ * Reducer used in create and edit tax record for storing state.
+ * ---------------------- 
+**/
+
+
+const taxRecordReducer = (state, action) => {
+    switch (action.type) {
+        case "SET_TITLE":
+            return { ...state, title: action.payload, titleError: "" };
+        case "SET_VALIDFROM":
+            return { ...state, validFrom: action.payload, validFromError: "" };
+        case "SET_VALIDTO":
+            return { ...state, validTo: action.payload, validToError: "" };
+        case "SET_DESCRIPTION": 
+            return { ...state, description: action.payload, descriptionError: "" };
+        case "SET_COST":
+            return { ...state, cost: action.payload, costError: "" };
+        case "SET_VEHICLE":
+            return {...state, vehicle : action.payload, vehicleError: ""};
+        case "SET_TITLE_ERROR":
+            return { ...state, titleError: action.payload };
+        case "SET_VALIDFROM_ERROR":
+            return { ...state, validFromError: action.payload };
+        case "SET_VALIDTO_ERROR":
+                return { ...state, validToError: action.payload };
+        case "SET_DESCRIPTION_ERROR":
+            return { ...state, descriptionError: action.payload };     
+        case "SET_COST_ERROR":
+            return { ...state, costError: action.payload };
+        case "SET_VEHICLE_ERROR":
+            return {...state, vehicleError : action.payload};
+        default:    
+            return state;
+        }
+}
+
+export default taxRecordReducer;
