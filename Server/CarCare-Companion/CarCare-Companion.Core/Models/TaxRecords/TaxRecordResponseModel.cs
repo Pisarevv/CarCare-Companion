4bd0fd98ac165fcc3d5167e36f025defa16121f2
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-20 15:32:50 +0300

    Add: new response model - TaxRecordResponseModel
    The purpose of this model is to transfer information about a record.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/TaxRecords/TaxRecordResponseModel.cs
@@ -0,0 +1,20 @@
+﻿namespace CarCare_Companion.Core.Models.TaxRecords;
+
+
+public class TaxRecordResponseModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public DateTime ValidFrom { get; set; }
+
+    public DateTime ValidTo { get; set; }
+
+    public decimal Cost { get; set; }
+
+    public string VehicleMake { get; set; } = null!;
+
+    public string VehicleModel { get; set;} = null!;
+
+}
