383c6a793e334a5d35c17cee99b1b6d178008f50
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-20 11:29:26 +0300

    Add: new request model - TaxRecordFormRequestModel
    Created a new request model - TaxRecordFormRequestModel, to manage data handling for the creating and editing of tax records.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/TaxRecords/TaxRecordFormRequestModel.cs
@@ -0,0 +1,33 @@
+﻿namespace CarCare_Companion.Core.Models.TaxRecords;
+
+using System.ComponentModel.DataAnnotations;
+
+using Microsoft.EntityFrameworkCore;
+
+
+using static Common.ValidationConstants.TaxRecord;
+
+public class TaxRecordFormRequestModel
+{
+
+    [Required]
+    [MinLength(MinTitleLength)]
+    [MaxLength(MaxTitleLength)]
+    public string Title { get; set; } = null!;
+
+    [Required]
+    public DateTime ValidFrom { get; set; }
+
+    [Required]
+    public DateTime ValidTo { get; set; }
+
+    public string? Description { get; set; }
+
+    [Required]
+    [Range(MinCost, MaxCost)]
+    [Precision(18, 2)]
+    public decimal Cost { get; set; }
+
+    [Required]
+    public string VehicleId { get; set; } = null!;
+}
