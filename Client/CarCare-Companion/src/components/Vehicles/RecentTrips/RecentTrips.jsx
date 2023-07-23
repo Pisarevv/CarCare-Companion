c6b4fe8bb8a0098016c1cebbefbc702242245beb
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-23 21:37:48 +0300

    Edit: component data retrieving
    Refractured the component to retrieve the data via a REST call.

--- Client/CarCare-Companion/src/components/Vehicles/RecentTrips/RecentTrips.jsx
+++ Client/CarCare-Companion/src/components/Vehicles/RecentTrips/RecentTrips.jsx
@@ -1,17 +1,47 @@
-import RecentTripCard from './RecentTripCard';
-import './RecentTrips.css'
+import { useEffect, useState } from 'react';
 
-const RecentTrips = ({recentTrips}) => {
+import { getLatestTrips } from '../../../services/tripService';
+
+import RecentTripCard from './RecentTripCard';
+
+import IsLoadingHOC from '../../Common/IsLoadingHoc';
+
+import './RecentTrips.css'
+const RecentTrips = (props) => {
+
+    const { setLoading } = props;
+
+    const [recentUserTrips, setRecentUserTrips] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const trips = await getLatestTrips(3);
+                setRecentUserTrips(recentUserTrips => trips);
+                setLoading(false);
+            }
+            catch (error) {
+                NotificationHandler(error)
+                setLoading(false);
+            }
+        })()
+    }, [])
 
     return (
         <div className="recent-trips-container">
             <div className="trips-header">Recent trips</div>
             <div className="trips-list">
-                {recentTrips && recentTrips.map(rt => <RecentTripCard key = {rt.id} details = {rt}/>)}
+                {
+                    recentUserTrips.length > 0
+                        ?
+                        recentUserTrips.map(rt => <RecentTripCard key = {rt.id} details = {rt}/> )
+                        :
+                        <div>You haven't added any trip records yet.</div>
+                }
             </div>
         </div>
     )
 }
 
 
-export default RecentTrips;
+export default IsLoadingHOC(RecentTrips);
