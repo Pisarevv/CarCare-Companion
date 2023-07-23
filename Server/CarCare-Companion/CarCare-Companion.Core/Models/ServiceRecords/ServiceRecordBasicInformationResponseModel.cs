3bea15226840686dcc524dbabd7496a32e933aeb
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-23 21:17:58 +0300

    Add: new response model
    Created a new response model - ServiceRecordBasicInformationResponseModel. This model will transfer basic information about a user service record.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/ServiceRecords/ServiceRecordBasicInformationResponseModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CarCare_Companion.Core.Models.ServiceRecords;
+
+public class ServiceRecordBasicInformationResponseModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public DateTime PerformedOn { get; set; }
+
+    public string VehicleMake { get; set; } = null!;
+
+    public string VehicleModel { get; set; } = null!;
+}
