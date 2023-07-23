75abd9d04b99835738795dcd85e7674f3929b323
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-23 21:48:19 +0300

    Add: new component RecentServiceCard
    Created a new functional component, RecentServiceCard. This component is responsible for rendering service records details in a card giving information about the title, date of performing and vehicle information.

---
+++ Client/CarCare-Companion/src/components/Vehicles/RecentServices/RecentServiceCard.jsx
@@ -0,0 +1,19 @@
+import ISODateStringToString from "../../../utils/ISODateStringToString";
+
+import './RecentServiceCard.css'
+
+const RecentServiceCard = ({details}) => {
+    
+    const {title ,performedOn,vehicleMake, vehicleModel} = details;
+    return (
+        
+        <div className="user-recent-trip-card-container">
+            <div className="recent-trip-card-border"></div>
+            <div>"{title}" performed on {ISODateStringToString.ddmmyyyy(performedOn)} on {vehicleMake} {vehicleModel} </div>
+            <div className="recent-trip-card-border"></div>
+        </div>
+
+    )
+}
+
+export default RecentServiceCard;
