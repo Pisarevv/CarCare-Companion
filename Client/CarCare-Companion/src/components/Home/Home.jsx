41489c345b46f6b7a57659dc924c7e6c0eb75006
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-23 21:33:34 +0300

    Edit: Home page component
    Removed all the content from the component.
    The component will be responsible for redirecting the user to the two new sub components - AuthenticatedHomePage and UnauthenticatedHomePage

--- Client/CarCare-Companion/src/components/Home/Home.jsx
+++ Client/CarCare-Companion/src/components/Home/Home.jsx
@@ -1,88 +1,24 @@
-import { NavLink } from "react-router-dom";
-
-import { useContext, useEffect, useState } from "react";
-
-import { NotificationHandler } from '../../utils/NotificationHandler'
-
-import IsLoadingHOC from '../Common/IsLoadingHoc';
-
+    
 import "./Home.css"
+import {  useAuthContext } from "../../contexts/AuthContext";
+import AuthenticatedHomePage from './AuthenticatedHomePage/AuthenticatedHomePage';
+import UnauthenticatedHomePage from './UnauthenticatedHomePage/UnauthenticatedHomePage';
 
-import { getAllCarouselAds } from "../../services/adService"; 
-import { AuthContext } from "../../contexts/AuthContext";
-import ReviewCard from "./ReviewCard";
 
 
-const Home = (props) => {
-
-    const  [carouselAds,setCarouselAds] = useState([]);
-
-    const { userLogout } = useContext(AuthContext);
-
-    const { setLoading } = props;
-
-    useEffect(() => {
-        (async () => {
-            try {
-                window.scrollTo(0, 0);
-                const result = await getAllCarouselAds();
-                setCarouselAds(carouselAds => result);
-                console.log(result);
-                setLoading(false);
-            }
-            catch (error) {
-                if (error === "Invalid access token") {
-                    NotificationHandler(error);
-                    userLogout();
-                    setLoading(false);
-                    
-                };
-            }
-        }
-        )()
-    },[])
-
+const Home = () => {
+    
+    const { isAuthenticated } = useAuthContext();
+    
     return (
         <section className="home">
 
             <div className="container">
-
-                <div className="introduction-container">
-                    <p>Welcome to CarCare Companion — your ultimate destination for all your car repair,trips and service management needs.</p>
-                    <p>Discover expert advice, valuable resources, and a vibrant community of automotive enthusiasts. Start exploring now!</p>
-                </div>
-
-                <div className="video-container">
-                    <video muted autoPlay loop src='video.mp4'></video>
-                </div>
-
-                <div className="join-container">
-                    <h2>Join Us Today!</h2>
-                    <p>Become a part of our dynamic community of car enthusiasts, mechanics, and DIYers.</p>
-                    <p> By joining us, you'll gain access to a wealth of knowledge, valuable tips, and engaging discussions on car repairs and service management.</p>
-                    <NavLink to="/Register">Sign up.</NavLink>
-                </div>
-
-                
-                <div className="review-container">
-                    <div className="cards">
-                        <div className="cards-inner">
-                             <div className="cards-border"></div>
-                             <div className="cards-row">
-                                {carouselAds.map(x => (
-                                    <div className="card-col" key={x.id}>
-                                     <ReviewCard  reviewInfo = {x}/>
-                                    </div>
-                                ))}
-                             </div>
-                        </div>
-                    </div>
-                </div>
-
+              { isAuthenticated ? <AuthenticatedHomePage/> : <UnauthenticatedHomePage/>   }
             </div>
 
         </section>
     )
 }
 
-export default IsLoadingHOC(Home); 
+export default Home; 
