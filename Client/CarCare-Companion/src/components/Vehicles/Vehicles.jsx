55fba0b3b1c672f7a68438ff764df19a6c00040c
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-23 21:49:37 +0300

    Minor changes

--- Client/CarCare-Companion/src/components/Vehicles/Vehicles.jsx
+++ Client/CarCare-Companion/src/components/Vehicles/Vehicles.jsx
@@ -11,26 +11,22 @@ import { NotificationHandler } from '../../utils/NotificationHandler'
 import IsLoadingHOC from '../Common/IsLoadingHoc';
 
 import './Vehicles.css';
-import { getLatestTrips } from '../../services/tripService';
+
 import RecentTrips from './RecentTrips/RecentTrips';
+import RecentServices from './RecentServices/RecentServices';
 
 const Vehicles = (props) => {
 
     const { setLoading } = props;
 
     const [userVehicles, setUserVehicles] = useState([]);
-    const [recentUserTrips, setRecentUserTrips] = useState([]);
 
-    console.log(userVehicles)
-    console.log(recentUserTrips)
 
     useEffect(() => {
         (async () => {
             try {
                 const vehicles = await getUserVehicles();
-                const trips = await getLatestTrips(3);
                 setUserVehicles(userVehicles => vehicles);
-                setRecentUserTrips(recentUserTrips => trips);
                 setLoading(false);
             }
             catch (error) {
@@ -46,8 +42,8 @@ const Vehicles = (props) => {
             <div className="vehicle-container">
                 <div className="user-recent-services-table">
                     <NavLink to="/Vehicle/Create">Add vehicle</NavLink>
-                    <RecentTrips recentTrips={recentUserTrips} />
-                    <div>test</div>
+                    <RecentTrips/>
+                    <RecentServices/>
                 </div>
                 <div className="user-vehicles"> {userVehicles.map(uv => <VehicleCard key={uv.id} vehicleData={uv} />)}</div>
             </div>  
