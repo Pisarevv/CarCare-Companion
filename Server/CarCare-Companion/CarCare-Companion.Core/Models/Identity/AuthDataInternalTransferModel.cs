89626af961384cec17155f6a6f2139c6a7607744
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-24 23:58:14 +0300

    Add: AuthDataInternalTransferModel
    Internal model that will transfer the refresh token, access token and user email on login.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Identity/AuthDataInternalTransferModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CarCare_Companion.Core.Models.Identity;
+
+public class AuthDataInternalTransferModel
+{
+    public string AccessToken { init; get; } = null!;
+
+    public string RefreshToken { init; get; } = null!;
+
+    public string Email { init; get; } = null!;
+}
