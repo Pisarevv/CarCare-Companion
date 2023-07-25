1489e533eb0b8db96ab261417d416602da1cb214
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-25 23:20:07 +0300

    Add: hook - useAxiosPrivate
    Implemented a new hook  - useAxiosPrivate, that will send authenticated requests to the server.
    The hook uses interceptors :
     - requestIntercept - handles the initial requests
    - responseIntercept - handles the JWT token refresh if one 401 response has occurred.

---
+++ Client/CarCare-Companion/src/hooks/useAxiosPrivate.jsx
@@ -0,0 +1,47 @@
+import { useEffect } from "react";
+
+import useRefreshToken from "./useRefreshToken";
+
+import { useAuthContext } from "../contexts/AuthContext";
+import { axiosPrivate } from "../api/axios/axios";
+
+const useAxiosPrivate = () => {
+    const refresh = useRefreshToken();
+    const { user, userLogout } = useAuthContext();
+
+    useEffect(() => {
+
+        const requestIntercept = axiosPrivate.interceptors.request.use(
+            config => {
+                if (!config.headers['Authorization']) {
+                    config.headers['Authorization'] = `Bearer ${user?.accessToken}`;
+                }
+                return config;
+            }, (error) => Promise.reject(error)
+        );
+
+        const responseIntercept = axiosPrivate.interceptors.response.use(
+            response => response,
+            async (error) => {
+                const prevRequest = error?.config;
+                if (error?.response?.status === 401 && !prevRequest?.sent) {
+                    prevRequest.sent = true;
+                    const newAccessToken = await refresh();
+                    prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+                    return axiosPrivate(prevRequest);
+                }
+                userLogout();
+                return Promise.reject(error);
+            }
+        );
+
+        return () => {
+            axiosPrivate.interceptors.request.eject(requestIntercept);
+            axiosPrivate.interceptors.response.eject(responseIntercept);
+        }
+    }, [user, refresh])
+
+    return axiosPrivate;
+}
+
+export default useAxiosPrivate;
