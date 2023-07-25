806e4f1eb484698cfb03be9b76d01c03be514e1f
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-25 23:25:15 +0300

    Edit: Home controller
    Implemented a refresh method to check whether the user is authenticated or not.

--- Client/CarCare-Companion/src/components/Home/Home.jsx
+++ Client/CarCare-Companion/src/components/Home/Home.jsx
@@ -1,24 +1,49 @@
-    
-import "./Home.css"
-import {  useAuthContext } from "../../contexts/AuthContext";
+import { useEffect } from 'react';
+
+import { useAuthContext } from "../../contexts/AuthContext";
+
 import AuthenticatedHomePage from './AuthenticatedHomePage/AuthenticatedHomePage';
 import UnauthenticatedHomePage from './UnauthenticatedHomePage/UnauthenticatedHomePage';
 
+import useRefreshToken from '../../hooks/useRefreshToken';
+import IsLoadingHOC from '../Common/IsLoadingHoc';
+
+import "./Home.css"
 
 
-const Home = () => {
-    
+
+const Home = (props) => {
+
+    const refresh = useRefreshToken();
     const { isAuthenticated } = useAuthContext();
-    
+    const { setLoading } = props;
+
+    useEffect(() => {
+        const refreshUser = async () => {
+            try {
+                await refresh();
+            } catch (err) {
+
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+
+        refreshUser();
+    }, [])
+
+
+
     return (
         <section className="home">
 
             <div className="container">
-              { isAuthenticated ? <AuthenticatedHomePage/> : <UnauthenticatedHomePage/>   }
+                {isAuthenticated ? <AuthenticatedHomePage /> : <UnauthenticatedHomePage />}
             </div>
 
         </section>
     )
 }
 
-export default Home; 
+export default IsLoadingHOC(Home); 
