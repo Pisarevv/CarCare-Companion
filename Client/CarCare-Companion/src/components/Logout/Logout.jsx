fc80cf0d8b6a69f450dd609696295cf19e9b289d
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-26 22:41:48 +0300

    Update: Login
    Login now sends a request to the server to terminate the refresh token and cleans the user state containing the JWT token.

--- Client/CarCare-Companion/src/components/Logout/Logout.jsx
+++ Client/CarCare-Companion/src/components/Logout/Logout.jsx
@@ -3,20 +3,40 @@ import { useNavigate } from "react-router-dom";
 
 import { useAuthContext } from "../../contexts/AuthContext";
 
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import IsLoadingHOC from "../Common/IsLoadingHoc";
+
+import { NotificationHandler } from "../../utils/NotificationHandler";
 
 
-const Logout =  () => {
+const Logout = (props) => {
 
     const {userLogout} = useAuthContext();
+    const {setLoading} = props;
     const navigate = useNavigate();
+    const axiosPrivate = useAxiosPrivate();
 
     useEffect(() => {
-        userLogout();
-        navigate("/");
-    },[])
+        const logoutUser = async () => {
+            try {
+                const response = await axiosPrivate.post('/Logout');
+            } catch (err) {
+                NotificationHandler(err);
+                navigate('/login', { state: { from: location }, replace: true });
+            }
+            finally{
+                userLogout();
+                navigate("/");
+                setLoading(false);
+            }
+        }
+        logoutUser();
+    
+    }, [])
+
 
     return null;
 }
 
 
-export default Logout;
+export default IsLoadingHOC(Logout);
