ff229f1601c3674ff31fa39d6204831589eb1ccb
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-26 22:40:16 +0300

    Minor change
    Change of properties names :
    vehiceType to vehicleTypeId
    fuelType to FuelTypeId

--- Client/CarCare-Companion/src/reducers/userVehicleReducer.jsx
+++ Client/CarCare-Companion/src/reducers/userVehicleReducer.jsx
@@ -11,24 +11,24 @@ const userVehicleReducer = (state, action) => {
             return { ...state, make: action.payload, makeError: "" };
         case "SET_MODEL":
             return { ...state, model: action.payload, modelError: "" };
-        case "SET_VEHICLETYPE":
-            return { ...state, vehicleType: action.payload, vehicleTypeError: "" };
+        case "SET_VEHICLETYPEID":
+            return { ...state, vehicleTypeId: action.payload, vehicleTypeError: "" };
         case "SET_MILEAGE":
             return { ...state, mileage: action.payload, mileageError: "" };
-        case "SET_FUELTYPE":
-            return { ...state, fuelType: action.payload, fuelTypeError: "" };
+        case "SET_FUELTYPEID":
+            return { ...state, fuelTypeId: action.payload, fuelTypeError: "" };
         case "SET_YEAR":
             return { ...state, year: action.payload, yearError: "" };
         case "SET_MAKE_ERROR":
             return { ...state, makeError: action.payload };
         case "SET_MODEL_ERROR":
             return { ...state, modelError: action.payload };
-        case "SET_VEHICLETYPE_ERROR":
-            return { ...state, vehicleTypeError: action.payload};
+        case "SET_VEHICLETYPEID_ERROR":
+            return { ...state, vehicleTypeIdError: action.payload};
         case "SET_MILEAGE_ERROR":
             return { ...state, mileageError: action.payload };
-        case "SET_FUELTYPE_ERROR":
-            return { ...state, fuelTypeError: action.payload };
+        case "SET_FUELTYPEID_ERROR":
+            return { ...state, fuelTypeIdError: action.payload };
         case "SET_YEAR_ERROR":
             return { ...state, yearError: action.payload };
         default:    
