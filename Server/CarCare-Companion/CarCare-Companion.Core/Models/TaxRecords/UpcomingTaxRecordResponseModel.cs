2384998e3000fe0877c076e65fd66c3bf109aa02
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-31 22:55:19 +0300

    Add: new response model - UpcomingTaxRecordResponseModel
    Created a new model that will contain information about upcoming user taxes

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/TaxRecords/UpcomingTaxRecordResponseModel.cs
@@ -0,0 +1,17 @@
+﻿namespace CarCare_Companion.Core.Models.TaxRecords;
+using System;
+
+
+public class UpcomingTaxRecordResponseModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public DateTime ValidTo { get; set; }
+
+    public string VehicleMake { get; set; } = null!;
+
+    public string VehicleModel { get; set; } = null!;
+
+}
