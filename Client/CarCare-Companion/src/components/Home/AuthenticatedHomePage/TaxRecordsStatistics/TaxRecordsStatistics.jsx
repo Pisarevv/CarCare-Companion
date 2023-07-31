be58840c56831ea6b251a11db0e3d62888feacdc
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-07-31 23:27:46 +0300

    Add: new component TaxRecordsStatistics
    Created a new functional component, TaxRecordsStatistics. This component is responsible for rendering details about the user tax records statistics.
    The rendered statistics are the user tax record count and total tax records cost.

---
+++ Client/CarCare-Companion/src/components/Home/AuthenticatedHomePage/TaxRecordsStatistics/TaxRecordsStatistics.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+
+import useAxiosPrivate from "../../../../hooks/useAxiosPrivate";
+import IsLoadingHOC from "../../../Common/IsLoadingHoc";
+
+import { NotificationHandler } from "../../../../utils/NotificationHandler";
+
+import './TaxRecordsStatistics.css'
+
+
+
+const TaxRecordsStatistics = (props) => {
+
+    const { setLoading } = props;
+
+    const navigate = useNavigate();
+
+    const axiosPrivate = useAxiosPrivate();
+
+    const [taxRecordsCount, setTaxRecordsCount] = useState(null);
+    const [taxRecordsCost, setTaxRecordsCost] = useState(null);
+
+
+    useEffect(() => {
+        let isMounted = true;
+        const controller = new AbortController();
+
+        const getRecordsStatistics = async () => {
+            try {
+                const requests = [
+                    axiosPrivate.get('/TaxRecords/Count', {
+                        signal: controller.signal
+                    }),
+                    axiosPrivate.get('/TaxRecords/Cost', {
+                        signal: controller.signal
+                    })
+                ];
+                Promise.all(requests)
+                .then(responses => {
+                    const taxRecordsCountResult = responses[0].data;
+                    const taxRecordsCostResult = responses[1].data;
+
+                    if(isMounted){
+                        setTaxRecordsCount(taxRecordsCount => taxRecordsCountResult);
+                        setTaxRecordsCost(taxRecordsCost => taxRecordsCostResult);
+                    }
+                })
+            } catch (err) {
+                NotificationHandler(err);
+                navigate('/login', { state: { from: location }, replace: true });
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+
+        getRecordsStatistics();
+
+        return () => {
+            isMounted = false;
+            isMounted && controller.abort();
+        }
+    }, [])
+
+
+    return (
+
+        <div className="home-page-tax-record-statistics-list">
+            <h4>Tax records:</h4>
+            <div className="home-page-tax-record-statistics-border"></div>
+            <div className="home-page-taxRecords-count">You have added {taxRecordsCount} 
+            {taxRecordsCount == 1 ? " record" : " records" } so far.</div>
+            <div className="home-page-taxRecors-cost">The total cost of your tax records is: {taxRecordsCost} lv.</div>
+          
+        </div>   
+        
+    )
+}
+
+
+export default IsLoadingHOC(TaxRecordsStatistics);
