5b21a936e1ae6bf1c0b7be08c5a16369ecb0f878
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-01 10:01:49 +0300

    Update: add property
    Added an role property to the authentication

--- Client/CarCare-Companion/src/hooks/useRefreshToken.jsx
+++ Client/CarCare-Companion/src/hooks/useRefreshToken.jsx
@@ -9,7 +9,7 @@ const useRefreshToken = () => {
             withCredentials: true
         });
         setAuth(prev => {
-            return { ...prev,email: response.data.email, accessToken: response.data.accessToken }
+            return { ...prev,email: response.data.email, role: response.data.role ,accessToken: response.data.accessToken }
         });
         return response.data.accessToken;
     }
