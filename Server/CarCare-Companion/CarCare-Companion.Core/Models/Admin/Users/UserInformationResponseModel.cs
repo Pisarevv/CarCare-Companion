3e52e7517a34583acbd2e02cee96851698d9448f
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-01 20:15:44 +0300

    Add: response model - UserInformationResponseModel
    Created a new response model - UserInformationResponseModel, that will contain details about an user of the application.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Admin/Users/UserInformationResponseModel.cs
@@ -0,0 +1,20 @@
+﻿namespace CarCare_Companion.Core.Models.Admin.Users;
+
+public class UserInformationResponseModel
+{
+    public string UserId { get; set; } = null!;
+
+    public string Username { get; set; } = null!;
+
+    public string? FirstName { get; set; } = null!;
+
+    public string? LastName { get; set; } = null!;
+
+    public int VehiclesCount { get; set; }
+
+    public int ServiceRecordsCount { get; set; }
+
+    public int TaxRecordsCount { get; set; }
+
+    public int TripsCount { get; set; }
+}
