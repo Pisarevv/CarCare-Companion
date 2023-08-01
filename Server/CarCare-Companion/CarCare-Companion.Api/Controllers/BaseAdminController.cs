d52a4d42e3c394a97f8e609450630aa3dadcfa50
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-01 19:50:28 +0300

    Add: base controller class - BaseAdminController
    Created a base controller that other admin controllers will inherit.
    The base controller uses role based authorization based on JWT token.

---
+++ Server/CarCare-Companion/CarCare-Companion.Api/Controllers/BaseAdminController.cs
@@ -0,0 +1,11 @@
+﻿namespace CarCare_Companion.Api.Controllers;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+[ApiController]
+public abstract class BaseAdminController : ControllerBase
+{
+}
