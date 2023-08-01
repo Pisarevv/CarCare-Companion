942fbb4a9a06972357d3424cf557f101385e123b
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-01 22:59:58 +0300

    Edit : model properties - UserInformationResponseModel
    Removed properties

--- Server/CarCare-Companion/CarCare-Companion.Core/Models/Admin/Users/UserInformationResponseModel.cs
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Admin/Users/UserInformationResponseModel.cs
@@ -6,15 +6,4 @@ public class UserInformationResponseModel
 
     public string Username { get; set; } = null!;
 
-    public string? FirstName { get; set; } = null!;
-
-    public string? LastName { get; set; } = null!;
-
-    public int VehiclesCount { get; set; }
-
-    public int ServiceRecordsCount { get; set; }
-
-    public int TaxRecordsCount { get; set; }
-
-    public int TripsCount { get; set; }
 }
