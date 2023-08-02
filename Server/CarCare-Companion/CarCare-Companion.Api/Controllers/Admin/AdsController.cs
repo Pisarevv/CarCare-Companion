614b8a1c88a201731654992aa635c42a6a07dc2c
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-02 21:06:41 +0300

    Add: new controller - AdsController and method - GetAll
    Created a new controlled - AdsController that will be accessible only by admins.
    It will contain methods for editing, creating and deleting carousel ads.
    
    Method :
    - GetAll - retrieves all the carousel ads after the validation process of the user is completed.

---
+++ Server/CarCare-Companion/CarCare-Companion.Api/Controllers/Admin/AdsController.cs
@@ -0,0 +1,73 @@
+﻿namespace CarCare_Companion.Api.Controllers.Admin;
+
+using CarCare_Companion.Common;
+using CarCare_Companion.Core.Contracts;
+using CarCare_Companion.Core.Models.Ads;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using static Common.GlobalConstants;
+using static Common.StatusResponses;
+
+[Route("[controller]")]
+
+public class AdsController : BaseAdminController
+{
+    private readonly IAdService adService;
+    private readonly IIdentityService identityService;
+    private readonly ILogger<AdsController> logger;
+
+    public AdsController(IAdService adService, IIdentityService identityService, ILogger<AdsController> logger)
+    {
+        this.adService = adService;
+        this.identityService = identityService;
+        this.logger = logger;
+    }
+
+    [HttpGet]
+    [Route("CarouselAds")]
+    [ProducesResponseType(200, Type = typeof(ICollection<CarouselAdResponseModel>))]
+    [ProducesResponseType(400, Type = typeof(ProblemDetails))]
+    [ProducesResponseType(403, Type = typeof(ProblemDetails))]
+    public async Task<IActionResult> GetAll()
+    {
+        try
+        {
+            string userId = this.User.GetId()!;
+
+            bool isUserAdministrator = await identityService.IsUserInRole(userId, AdministratorRoleName);
+
+
+            if (!isUserAdministrator)
+            {
+                return StatusCode(403, new ProblemDetails()
+                {
+                    Detail = StatusResponses.BadRequest,
+
+                });
+            }
+
+            ICollection<CarouselAdResponseModel> carouselAds = await adService.GetAllAsync();
+
+            return StatusCode(200, carouselAds);
+        }
+        catch (SqlException ex)
+        {
+            logger.LogWarning(ex.Message);
+            return StatusCode(400, new ProblemDetails()
+            {
+                Detail = StatusResponses.BadRequest,
+
+            });
+        }
+        catch (Exception ex)
+        {
+            logger.LogInformation(ex.Message);
+            return StatusCode(400, new ProblemDetails()
+            {
+                Detail = InvalidData,
+
+            });
+        }
+
+    }
+}
