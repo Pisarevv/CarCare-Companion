10555dd2c2ab0f948957ff0f1f10062535752fcf
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-03 09:19:36 +0300

    Add: new request model - CarouselAdFromRequestModel
    Created a new request model - CarouselAdFromRequestModel, that will be used data transfer in creating and editing carousel ads.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Ads/CarouselAdFromRequestModel.cs
@@ -0,0 +1,24 @@
+﻿namespace CarCare_Companion.Core.Models.Ads;
+
+using System.ComponentModel.DataAnnotations;
+
+using static Common.ValidationConstants.CarouselAdModel;
+
+public class CarouselAdFromRequestModel
+{
+    public string Id { get; set; } = null!;
+
+    [Required]
+    [MinLength(MinNameLength)]
+    [MaxLength(MaxNameLength)]
+    public string UserFirstName { get; set; } = null!;
+
+    [Required]
+    [MinLength(MinDescriptionLength)]
+    [MaxLength(MaxDescriptionLength)]
+    public string Description { get; set; } = null!;
+
+    [Required]
+    [Range(MinStarsRating, MaxStarsRating)]
+    public int StarsRating { get; set; }
+}
