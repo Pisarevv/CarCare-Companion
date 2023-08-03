295cc62f86bf1e1997b83e0d380831c8b1b561c3
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-03 21:47:33 +0300

    Add: new email sender - GmailEmailSender
    Implemented a new email sender - GmailEmailSender, that uses the Gmail SMTP server to send emails.
    It receives its settings from dependency injection.
    The methods that the sender contains:
    1.SendEmailAsync - this method receives a MailRequest model and sends a single email
    2. SendEmailsAsync - this method receives a collection of MailRequests model and sends multiple emails

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Messaging/GmailEmailSender.cs
@@ -0,0 +1,68 @@
+﻿namespace CarCare_Companion.Core.Messaging;
+
+using CarCare_Companion.Api.Extensions.Mail;
+using CarCare_Companion.Core.Messaging.Models;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
+using MimeKit;
+using System.Threading.Tasks;
+
+public class GmailEmailSender : IEmailSender
+{
+    private readonly MailSettings mailSettings;
+    public GmailEmailSender(IOptions<MailSettings> mailSettings) 
+    {
+        this.mailSettings = mailSettings.Value;
+    }
+
+    public async Task SendEmailAsync(MailRequest mailRequest)
+    {
+        var email = new MimeMessage();
+        email.Sender = MailboxAddress.Parse(mailSettings.Mail);
+        email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+        email.Subject = mailRequest.Subject;
+        var builder = new BodyBuilder();
+       
+        builder.HtmlBody = mailRequest.Body;
+        email.Body = builder.ToMessageBody();
+        using (var smtp = new SmtpClient())
+        {
+            smtp.Connect(mailSettings.Host, mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(mailSettings.Mail, mailSettings.Password);
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
+        }
+          
+    }
+
+    public async Task SendEmailsAsync(ICollection<MailRequest> mailRequests)
+    {
+        var emails = new List<MimeMessage>();
+
+        foreach (var mailRequest in mailRequests)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+            email.Subject = mailRequest.Subject;
+            var builder = new BodyBuilder();
+
+            builder.HtmlBody = mailRequest.Body;
+            email.Body = builder.ToMessageBody();
+
+            emails.Add(email);
+        }
+
+        using (var smtp = new SmtpClient())
+        {
+            smtp.Connect(mailSettings.Host, mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(mailSettings.Mail, mailSettings.Password);
+            foreach(var email in emails)
+            {
+                smtp.Send(email);
+            }
+            smtp.Disconnect(true);
+        }
+    }
+}
