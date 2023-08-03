2fb2b7532945a882331770a09876b296d48351ae
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-03 21:48:54 +0300

    Add: new model - MailRequest
    Created a new model - MailRequest, that will hold the email information and transfer it.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Messaging/Models/MailRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace CarCare_Companion.Core.Messaging.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MailRequest
+{
+    public string ToEmail { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+}
