8d4c3d16d0ca4cf278fb7f3b795fb1479a88ff18
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-03 21:46:02 +0300

    Add: Messaging to project
    Created a interface for email senders that contains two methods :
    
    1.SendEmailAsync - this method receives a MailRequest model and sends a single email
    2. SendEmailsAsync - this method receives a collection of  MailRequests model and sends multiple emails

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Messaging/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿namespace CarCare_Companion.Core.Messaging;
+
+using System.Threading.Tasks;
+
+using CarCare_Companion.Core.Messaging.Models;
+
+
+public interface IEmailSender
+{
+    public Task SendEmailAsync(MailRequest mailRequest);
+
+    public Task SendEmailsAsync(ICollection<MailRequest> mailRequests);
+}
