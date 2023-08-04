ca5293b482da6edbfd47829cd77743e3aad338ed
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-04 15:39:53 +0300

    Add: new class CacheKeysAndDurations
    Added a new static class - CacheKeysAndDurations, that will contain the cache keys and durations for different objects

---
+++ Server/CarCare-Companion/CarCare-Companion.Common/CacheKeysAndDurations.cs
@@ -0,0 +1,15 @@
+﻿namespace CarCare_Companion.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class CacheKeysAndDurations
+{
+    public static class CarouselAds
+    {
+        public static string CarouselAdsCacheKey = "CarouselAds";
+        public static int CarouselAdsCacheDurationMinutes = 10;
+    }
+}
