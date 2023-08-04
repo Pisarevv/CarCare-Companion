d2ba5a04ad4c322dbc4245d1e6bc44652d22cfbb
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-04 13:44:08 +0300

    Add: TemplatePaths class
    Created a static class - TemplatePaths, that stores paths to different email templates.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Messaging/EmailTemplates/TemplatePaths.cs
@@ -0,0 +1,6 @@
+﻿namespace CarCare_Companion.Core.Messaging.EmailTemplates;
+
+public static class TemplatePaths
+{
+    public static string TaxRecordExpiryNoticePath = @"..\..\..\..\CarCare-Companion.Core\Messaging\EmailTemplates\TaxRecordExpiryNotice.html";
+}
