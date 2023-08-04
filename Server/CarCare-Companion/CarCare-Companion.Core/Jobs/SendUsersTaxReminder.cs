36f476304d14fd6b6d6bb780a377d94086a9ea1b
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-04 13:49:31 +0300

    Add: new job - SendUsersTaxReminder
    Ceated a new job -SendUsersTaxReminder, that will be execured daily to send emails about expiring user taxes.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Jobs/SendUsersTaxReminder.cs
@@ -0,0 +1,36 @@
+﻿namespace CarCare_Companion.Api.Jobs;
+
+using Microsoft.Extensions.Logging;
+
+using Quartz;
+
+using CarCare_Companion.Core.Contracts;
+
+
+public class SendUsersTaxReminder : IJob
+{
+    private readonly ILogger<SendUsersTaxReminder> logger;
+    private readonly IMessagingService messagingService;
+
+    public SendUsersTaxReminder(ILogger<SendUsersTaxReminder> logger, IMessagingService messagingService)
+    {
+        this.logger = logger;
+        this.messagingService = messagingService;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    { 
+        logger.LogInformation($"Starting executing tax reminder messaging job at {DateTime.Now}");
+        try
+        {
+            await messagingService.SendTaxReminderEmailsToUsers();
+            logger.LogInformation($"Executed tax reminder messaging job at {DateTime.Now}");
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"Executing tax reminder messaging job failed at {DateTime.Now}", ex.Message);
+        }
+
+        return;
+    }
+}
