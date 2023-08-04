1bbde303da93d3496616b5d23ffb3d9265d28314
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-04 13:44:53 +0300

    Add: new response model - UpcomingUserTaxResponseModel
    Created a new response model - UpcomingUserTaxResponseModel, that contains the data about an expiring tax of the user.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/TaxRecords/UpcomingUserTaxResponseModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CarCare_Companion.Core.Models.TaxRecords;
+
+
+public class UpcomingUserTaxResponseModel
+{
+    public string Email { get; set; } = null!;
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set;}
+
+    public string TaxName { get; set; } = null!;
+
+    public string TaxValidTo { get; set; } = null!;
+
+    public string VehicleMake { get; set; } = null!;
+
+    public string VehicleModel { get; set; } = null!;
+}
