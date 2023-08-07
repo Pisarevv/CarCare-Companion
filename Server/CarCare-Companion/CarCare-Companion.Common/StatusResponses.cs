38f21d2d6f3fe36fac0e02338fef719c37cd544c
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-07 21:50:02 +0300

    Minor change

--- Server/CarCare-Companion/CarCare-Companion.Common/StatusResponses.cs
+++ Server/CarCare-Companion/CarCare-Companion.Common/StatusResponses.cs
@@ -2,19 +2,33 @@
 public static class StatusResponses
 {
     public const string GenericError = "An error occurred while processing your request. Please try again later.";
-    public const string NoPermission = "No permission";
+
+    public const string NoPermission = "You do not have the necessary permissions to access this resource.";
+
     public const string TokenExpired = "Token expired";
+
     public const string InvalidPassword = "Invalid password";
+
     public const string InvalidUser = "User does not exist";
-    public const string InvalidData = "Invalid data";
+
+    public const string InvalidData = "Invalid data provided.";
+
     public const string InvalidCredentials = "Login or password don't match";
+
     public const string MissingOrInvalidFields = "Invalid input fields";
+
     public const string UserEmailAlreadyExists = "A user with the same email already exists";
+
     public const string BadRequest = "Bad request";
+
     public const string FileSizeTooBig = "The size of the file is too big";
+
     public const string ResourceNotFound = "The resource does not exist";
+
     public const string Success = "Success";
+
     public const string AlreadyAdmin = "The user is already an administrator";
+
 
 
 }
