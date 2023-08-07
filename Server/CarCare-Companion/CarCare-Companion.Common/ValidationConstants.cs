300b555f63fd6bd5e761fab23eeb11fee67c26eb
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-07 20:35:29 +0300

    Minor change in validations

--- Server/CarCare-Companion/CarCare-Companion.Common/ValidationConstants.cs
+++ Server/CarCare-Companion/CarCare-Companion.Common/ValidationConstants.cs
@@ -4,27 +4,27 @@
     {
         public static class ApplicationUser
         {
-            public const int MinFirstNameLength = 2;
+            public const int MinFirstNameLength = 1;
             public const int MaxFirstNameLength = 50;
-            public const int MinLastNameLength = 2;
+            public const int MinLastNameLength = 1;
             public const int MaxLastNameLength = 50;
         }
 
         public static class FuelType 
         {
-            public const int MinNameLength = 2;
+            public const int MinNameLength = 1;
             public const int MaxNameLength = 20;
         }
 
         public static class VehicleType
         {
-            public const int MinNameLength = 2;
+            public const int MinNameLength = 1;
             public const int MaxNameLength = 20;
         }
 
         public static class ServiceRecord
         {
-            public const int MinTitleLength = 2;
+            public const int MinTitleLength = 1;
             public const int MaxTitleLength = 50;
             public const double MinMileage = 0;
             public const double MaxMileage = double.MaxValue;
@@ -35,7 +35,7 @@
 
         public static class TaxRecord
         {
-            public const int MinTitleLength = 2;
+            public const int MinTitleLength = 1;
             public const int MaxTitleLength = 50;
             public const double MinCost = 0;
             public const double MaxCost = double.MaxValue;
@@ -43,9 +43,9 @@
 
         public static class TripRecord
         {
-            public const int MinStartDestinationNameLength = 2;
+            public const int MinStartDestinationNameLength = 1;
             public const int MaxStartDestinationNameLength = 100;
-            public const int MinEndDestinationNameLength = 2;
+            public const int MinEndDestinationNameLength = 1;
             public const int MaxEndDestinationNameLength = 100;
             public const double MinTravelledRange = 0;
             public const double MaxTravelledRange = 10000;
@@ -57,9 +57,9 @@
 
         public static class Vehicle
         {
-            public const int MinMakeNameLength = 2;
+            public const int MinMakeNameLength = 1;
             public const int MaxMakeNameLength = 50;
-            public const int MinModelNameLength = 2;
+            public const int MinModelNameLength = 1;
             public const int MaxModelNameLength = 50;
             public const double MinMileage = 0;
             public const double MaxMileage = 999999999;
@@ -69,7 +69,7 @@
 
         public static class CarouselAdModel
         {
-            public const int MinNameLength = 2;
+            public const int MinNameLength = 1;
             public const int MaxNameLength = 50;
             public const int MinDescriptionLength = 10;
             public const int MaxDescriptionLength = 500;
