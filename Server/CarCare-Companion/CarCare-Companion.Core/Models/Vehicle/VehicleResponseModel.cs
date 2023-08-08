cadcd5bced3b8897a577a9c618bbe24dd14c5b79
Hristo Pisarev <hristo.pisarev98@gmail.com>
2023-08-08 15:35:28 +0300

    Add: new response model - VehicleResponseModel
    Created a new model - VehicleResponseModel, that will be used to return data on vehicle create and update

--- Server/CarCare-Companion/CarCare-Companion.Core/Models/Vehicle/VehicleResponseModel.cs
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Vehicle/VehicleResponseModel.cs
@@ -1,22 +1,19 @@
 ﻿namespace CarCare_Companion.Core.Models.Vehicle;
 
-using System.ComponentModel.DataAnnotations;
 
-
-
-public class VehicleEditResponseModel
+public class VehicleResponseModel
 {
+    public string Id { get; set; } = null!;
+
     public string Make { get; set; } = null!;
 
     public string Model { get; set; } = null!;
 
     public double Mileage { get; set; }
 
-
     public int Year { get; set; }
 
     public int FuelTypeId { get; set; }
-
 
     public int VehicleTypeId { get; set; }
 
