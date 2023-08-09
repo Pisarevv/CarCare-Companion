8e97ef9e9fadb1245d5662fe8410aac7d7c56c0b
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-08-09 23:23:39 +0300

    Update: NotificationHandler
    Changed the functionality of the component to show errors based on status response codes.

--- Client/CarCare-Companion/src/utils/NotificationHandler.jsx
+++ Client/CarCare-Companion/src/utils/NotificationHandler.jsx
@@ -5,61 +5,32 @@
  * -----------------
 **/
 
-import { Store } from 'react-notifications-component';
 
-const notificationMessages = {
-    "Invalid access token" : {
-        title : "Invalid access token",
-        message : "Your session has expired. Please log in again.",
-        type : "warning"
-    },
-    "Login or password don't match" : {
-        title : "Invalid credentials",
-        message : "Login or password don't match",
-        type : "warning"
-    },
-    "One or more validation errors occurred." : {
-        title : "Invalid input fields",
-        message : "Please make sure all inputs are filled with valid information before submiting",
-        type : "warning"
-    },
-    "Invalid input fields" : {
-        title : "Invalid input fields",
-        message : "Please make sure all inputs are valid before submiting",
-        type : "warning"
-    },
-    "A user with the same email already exists" : {
-        title : "A user with the same email already exists",
-        message : "Please use another email to register",
-        type : "warning"
-    },
-    "Comment cannot be empty" : {
-        title : "Comment field cannot be empty",
-        message : "Please write your comment befor submiting",
-        type : "warning"
-    },
-    "Generic error" : {
-        title : "Unexpected error",
-        message : "Something unexpected happend. Please refresh the page.",
-        type : "danger"
-    },
-    "Successful record removal" : {
-        title : "Sucess",
-        message : "Sucessfully removed the record.",
-        type : "success"
+const responseStatusNotificationType = (responeStatus) => {
+    if(Number(responeStatus) >= 100 && Number(responeStatus) <= 199){
+        return "info";
     }
+
+    if(Number(responeStatus) >= 200 && Number(responeStatus) <= 299){
+        return "success";
+    }
+
+    if(Number(responeStatus) >= 300 && Number(responeStatus) <= 399){
+        return "default";
+    }
+
+    if(Number(responeStatus) >= 400 && Number(responeStatus) <= 599){
+        return "warning";
+    }
+
 }
 
+'success' | 'danger' | '' | 'default' | 'warning';
 
-
-
-export const NotificationHandler = (input) => {
-
-    if(!notificationMessages.hasOwnProperty(input)){
-       input = "Generic error"
-    }
+export const NotificationHandler = (title,message,responeStatus) => {
     
-    const {title,message,type} = notificationMessages[input];
+    const type = responseStatusNotificationType(responeStatus);
+    
     return (
         Store.addNotification({
             title: title,
