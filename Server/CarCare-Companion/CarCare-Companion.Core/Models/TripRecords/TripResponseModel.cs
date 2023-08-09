fca1fd10f5646e3b33b411003d2e61b558303720
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-08-09 09:43:11 +0300

    Add: new response model - TripResponseModel
    Created a new response model -TripResponseModel. It will transfer data on create and edit methods.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/TripRecords/TripResponseModel.cs
@@ -0,0 +1,17 @@
+﻿namespace CarCare_Companion.Core.Models.TripRecords;
+
+
+public class TripResponseModel
+{
+    public string Id { get; set; } = null!;
+
+    public string StartDestination { get; set; } = null!;
+
+    public string EndDestination { get; set; } = null!;
+
+    public double MileageTravelled { get; set; }
+
+    public double? UsedFuel { get; set; }
+
+    public decimal? FuelPrice { get; set; }
+}
