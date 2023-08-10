7348d9a12a0e403870dd042df9779c1c0d02fc7f
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-08-10 15:51:33 +0300

    Add: new response model - ServiceRecordResponseModel
    Created a new response model - ServiceRecordResponseModel, that will be used for response after creating or editing a service record.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/ServiceRecords/ServiceRecordResponseModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CarCare_Companion.Core.Models.ServiceRecords;
+
+public class ServiceRecordResponseModel
+{
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public DateTime PerformedOn { get; set; }
+
+    public double Mileage { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal Cost { get; set; }
+
+    public string VehicleId { get; set; } = null!;
+
+}
