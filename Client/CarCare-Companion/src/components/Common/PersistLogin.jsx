96787e91380e674bf65ec42606130ef84538bc9e
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-08-11 14:59:55 +0300

    Update: Documentation to PersistLogin component

--- Client/CarCare-Companion/src/components/Common/PersistLogin.jsx
+++ Client/CarCare-Companion/src/components/Common/PersistLogin.jsx
@@ -1,35 +1,53 @@
 import { Outlet } from "react-router-dom";
-
 import { useState, useEffect } from "react";
-
 import useRefreshToken from "../../hooks/useRefreshToken";
-
 import { useAuthContext } from "../../contexts/AuthContext";
 
+/**
+ * PersistLogin Component
+ *
+ * This component aims to maintain user sessions across page refreshes.
+ * When the component is mounted, it checks the authentication status.
+ * If the user is not authenticated, it tries to refresh the token.
+ * Only when these operations are completed, the child routes (via Outlet) will be rendered.
+ * 
+ * Children components (routes) should be wrapped by this component to benefit from 
+ * persistent login capabilities.
+ */
 const PersistLogin = () => {
-    const [isLoading,setIsLoading] = useState(true);
+    // State to track loading status.
+    const [isLoading, setIsLoading] = useState(true);
+
+    // Hooks to manage authentication.
     const refresh = useRefreshToken();
-    const {isAuthenticated} = useAuthContext();
+    const { isAuthenticated } = useAuthContext();
 
     useEffect(() => {
+        /**
+         * verifyRefreshToken Function
+         *
+         * If the user is not authenticated, this function will try to refresh the token.
+         * Regardless of the result (success or failure), the loading state will be set to false.
+         */
         const verifyRefreshToken = async () => {
-            try{
+            try {
                 await refresh();
-            }
-            catch(err){
-              
-            }
-            finally{
+            } catch (err) {
+                // Handle the error appropriately, e.g., you might want to log the error
+                // or show a notification to the user.
+            } finally {
                 setIsLoading(false);
             }
         }
-      
+
+        // Check if the user is authenticated. If not, attempt to refresh the token.
         !isAuthenticated ? verifyRefreshToken() : setIsLoading(false);
     }, [])
 
     return (
         <>
-        { isLoading ? "" : <Outlet/>  }
+            {/* Only render the Outlet (children routes) when not loading */}
+            {isLoading ? "" : <Outlet />}
         </>
     )
 }
