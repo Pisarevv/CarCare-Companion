89fab84366a01e149c8edc31685d7e791815521e
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-08-11 23:51:38 +0300

    Add: ImageService documentation

--- Server/CarCare-Companion/CarCare-Companion.Core/Contracts/IImageService.cs
+++ Server/CarCare-Companion/CarCare-Companion.Core/Contracts/IImageService.cs
@@ -4,7 +4,17 @@ using Microsoft.AspNetCore.Http;
 
 public interface IImageService
 {
+    /// <summary>
+    /// Uploads a vehicle image asynchronously.
+    /// </summary>
+    /// <param name="file">The image file to upload.</param>
+    /// <returns>The URL or identifier of the uploaded image.</returns>
     public Task<string> UploadVehicleImage(IFormFile file);
 
+    /// <summary>
+    /// Retrieves the URL of a vehicle image based on its key asynchronously.
+    /// </summary>
+    /// <param name="stringKey">The key or identifier of the image.</param>
+    /// <returns>The URL of the image.</returns>
     public Task<string> GetImageUrlAsync(string stringKey);
 }
