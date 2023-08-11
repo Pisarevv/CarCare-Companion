c31c1e1758d69b86ebc30c80978445d44a1f7338
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-08-11 23:53:19 +0300

    Add: UserService documentation

--- Server/CarCare-Companion/CarCare-Companion.Core/Contracts/IUserService.cs
+++ Server/CarCare-Companion/CarCare-Companion.Core/Contracts/IUserService.cs
@@ -4,8 +4,16 @@ using CarCare_Companion.Core.Models.Admin.Users;
 
 public interface IUserService
 {
+    /// <summary>
+    /// Retrieves all users asynchronously.
+    /// </summary>
+    /// <returns>A collection of UserInformationResponseModels representing the users.</returns>
     public Task<ICollection<UserInformationResponseModel>> GetAllUsersAsync();
 
+    /// <summary>
+    /// Retrieves detailed information of a specific user based on its ID asynchronously.
+    /// </summary>
+    /// <param name="userId">The ID of the user to retrieve details for.</param>
+    /// <returns>The UserDetailsResponseModel containing the user's details.</returns>
     public Task<UserDetailsResponseModel> GetUserDetailsByIdAsync(string userId);
-
 }
