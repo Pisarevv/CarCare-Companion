9fb0ccb6bdfa8cedf861f3f9611a09c4bde3dcbd
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-08-12 13:11:58 +0300

    Add: new components - Unauthorized and NotFound
    Added two new components:
    - Unauthorized - this page will be shown when users want to access pages that they are not authorized
    - NotFound - this page will be shown when users want to access pages that do not exist

---
+++ Client/CarCare-Companion/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,14 @@
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+       <section className="not-found-section">
+         <div className="not-found-container">
+            <h1>Oops! You seem to be lost.</h1>
+            <h1>404 Not Found</h1>
+        </div>
+       </section>
+    )
+}
+
+export default NotFound;
---
+++ Client/CarCare-Companion/src/components/Unauthorized/Unauthorized.jsx
@@ -0,0 +1,14 @@
+import './Unauthorized.css';
+
+const Unauthorized = () => {
+    return (
+       <section className="unauthorized-section">
+         <div className="unauthorized-container">
+            <h1>Hey you! Stop right there.</h1>
+            <h1>401 Unauthorized</h1>
+        </div>
+       </section>
+    )
+}
+
+export default Unauthorized;
