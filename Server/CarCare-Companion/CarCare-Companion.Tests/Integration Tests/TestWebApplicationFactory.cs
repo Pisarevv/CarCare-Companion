10a721dabfb8b50c6123b30aca6eaca6512b54c4
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-08-23 22:22:23 +0300

    Add: TestWebApplicationFactory
    Crated a custom WebApplicationFactory that will be used for integration tests.
    It loads the configuration for the test enviroment and loads the test database connection string.

---
+++ Server/CarCare-Companion/CarCare-Companion.Tests/Integration Tests/TestWebApplicationFactory.cs
@@ -0,0 +1,34 @@
+﻿namespace CarCare_Companion.Tests.Integration_Tests;
+
+using CarCare_Companion.Infrastructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public class TestWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+
+        builder.ConfigureServices((services) =>
+        {
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Test.json")
+            .Build();
+
+            var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<CarCareCompanionDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+            services.AddDbContext<CarCareCompanionDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("TestDatabaseConnection"));
+            });
+        });
+    }
+}
