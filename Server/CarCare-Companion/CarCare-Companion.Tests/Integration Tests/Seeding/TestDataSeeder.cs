8fc3ddecde77bb0932a2044a506db81a24e970cc
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-08-27 16:40:07 +0300

    Add: Test data seeders

---
+++ Server/CarCare-Companion/CarCare-Companion.Tests/Integration Tests/Seeding/TestDataSeeder.cs
@@ -0,0 +1,43 @@
+﻿namespace CarCare_Companion.Tests.Integration_Tests.Seeding;
+
+using CarCare_Companion.Infrastructure.Data;
+using CarCare_Companion.Infrastructure.Data.Seeding;
+using CarCare_Companion.Tests.Integration_Tests.Seeding.Seeders;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+
+public class TestDataSeeder : ISeeder
+{
+    public async Task SeedAsync(CarCareCompanionDbContext dbContext, IServiceProvider serviceProvider)
+    {
+        if (dbContext == null)
+        {
+            throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        if (serviceProvider == null)
+        {
+            throw new ArgumentNullException(nameof(serviceProvider));
+        }
+
+        var logger = serviceProvider.GetService<ILoggerFactory>().CreateLogger(typeof(TestDataSeeder));
+
+        var seeders = new List<ISeeder>
+                          {
+                              new TestApplicationUsersSeeder(),
+                              new TestVehicleSeeder(),
+                              new TestTaxRecordSeeder(),
+                              new TestTripRecordSeeder(),
+                              new TestServiceRecordsSeeder(),
+                          };
+
+        foreach (var seeder in seeders)
+        {
+            await seeder.SeedAsync(dbContext, serviceProvider);
+            await dbContext.SaveChangesAsync();
+            logger.LogInformation($"Seeder {seeder.GetType().Name} done.");
+        }
+    }
+}
