c421e4abfe75b16c1833962c0e4cc45868c491e1
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-08-27 14:55:31 +0300

    Create: TestDataConstants
    Created a new class - TestDataConstants, that holds users and vehicles ids

---
+++ Server/CarCare-Companion/CarCare-Companion.Tests/Integration Tests/Seeding/TestDataConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace CarCare_Companion.Tests.Integration_Tests.Common;
+
+public static class TestDataConstants
+{
+    public static string User1Id = "cded6a6d-8c93-4de5-ae10-0f7c889430a4";
+    public static string User1VehicleId = "4c266e97-63db-4f7e-bb42-ea1a229a6e17";
+
+    public static string User2Id = "276be7ad-b4df-404d-b23e-96a9dcdb3e9b";
+    public static string User2VehicleId = "97b72442-6f0d-43d8-9b35-21525f813827";
+
+    public static string User3Id = "41f9f2f1-3d51-4b01-a335-e5bfb07f2b53";
+    public static string User3VehicleId = "e2377d0b-6d7b-4a81-a0fe-d4eac0122e53";
+
+    public static string User4Id = "161f4a82-6c11-45eb-bd3b-98e1f88ddc45";
+    public static string User4VehicleId = "bfbb2ed1-e21f-41f8-b94e-3ddc34e345b6";
+
+}
