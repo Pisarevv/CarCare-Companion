a872a8926653e18950e03156c1620a06dd5d91cf
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-09-04 15:55:25 +0300

    Add: Test for editing service record with non existing vehicle

--- Server/CarCare-Companion/CarCare-Companion.Tests/Integration Tests/ServiceRecordsControllerTests.cs
+++ Server/CarCare-Companion/CarCare-Companion.Tests/Integration Tests/ServiceRecordsControllerTests.cs
@@ -522,6 +522,46 @@ public class ServiceRecordsControllerTests
         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
     }
 
+    //Test the PATCH end point for editing a service record with valid data but non existing vehicle
+    [Test]
+    public async Task PATCH_Edit_ReturnsStatusCode403_WhenVehicle_DoesntExist()
+    {
+        //Assert 
+        string vehicleId = "879335f9-d671-46a0-bbe3-a8e2eb9089f9";
+
+        ICollection<string> userRoles = new HashSet<string>();
+        ICollection<Claim> claims = new List<Claim>();
+        Vehicle userVehicle = Vehicles.Where(v => v.OwnerId == users[0].Id).First();
+        string serviceRecordToEditId = ServiceRecords.Where(sr => sr.OwnerId == users[0].Id).First().Id.ToString();
+
+        var rawToken = jwtService.GenerateJwtToken(claims);
+        string token = new JwtSecurityTokenHandler().WriteToken(rawToken);
+
+        ServiceRecordFormRequestModel editedRecord = new ServiceRecordFormRequestModel()
+        {
+            Title = "",
+            Cost = 10,
+            Description = "EditedDescription",
+            Mileage = 1506,
+            PerformedOn = DateTime.Now,
+            VehicleId = userVehicle.Id.ToString(),
+        };
+
+        var recordJson = JsonConvert.SerializeObject(editedRecord);
+
+        var request = new HttpRequestMessage(HttpMethod.Patch, $"/ServiceRecords/Edit/{serviceRecordToEditId}");
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        request.Content = new StringContent(recordJson, Encoding.UTF8, "application/json");
+
+
+        //Act
+        var response = await client.SendAsync(request);
+
+
+        //Assert
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
+    }
+
 
     [TearDown]
     public void TearDown()
