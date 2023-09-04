b59e904b8459b99952ccf74a538f0f517f9213cd
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-09-04 22:46:16 +0300

    Add: ISearchService
    Added a service that will be responsible for records searching.

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Contracts/ISearchService.cs
@@ -0,0 +1,9 @@
+﻿namespace CarCare_Companion.Core.Contracts;
+
+using CarCare_Companion.Core.Enums;
+using CarCare_Companion.Core.Models.Search;
+
+public interface ISearchService 
+{
+    public RecordsQueryModel Search(string userId, string? category = null, string? searchTerm = null, RecordsSorting sorting = RecordsSorting.Newest, int currentPage = 1, int recordsPerPage = 1);
+}
