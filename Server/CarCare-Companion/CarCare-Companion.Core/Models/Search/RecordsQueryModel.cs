079279205602583d8355f29971dd9c58644bdfd5
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-09-06 19:08:22 +0300

    Add: RecordsQueryModel
    Created a new model - RecordsQueryModel, that will be used for transferring filtered data about the searched records

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Search/RecordsQueryModel.cs
@@ -0,0 +1,24 @@
+﻿namespace CarCare_Companion.Core.Models.Search;
+
+using CarCare_Companion.Core.Models.ServiceRecords;
+using CarCare_Companion.Core.Models.TaxRecords;
+using CarCare_Companion.Core.Models.Trip;
+
+public class RecordsQueryModel
+{
+
+    public RecordsQueryModel()
+    {
+        this.TaxRecords = new List<TaxRecordDetailsResponseModel>();
+        this.TripRecords = new List<TripDetailsByUserResponseModel>();
+        this.ServiceRecords = new List<ServiceRecordDetailsResponseModel>();
+    }
+
+    public int TotalRecordsCount { get; set; }
+
+    public ICollection<TaxRecordDetailsResponseModel> TaxRecords { get; set; }
+
+    public ICollection<TripDetailsByUserResponseModel> TripRecords { get; set; }
+
+    public ICollection<ServiceRecordDetailsResponseModel> ServiceRecords { get; set; }
+}
