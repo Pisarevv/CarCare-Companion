df11a1a9bdc77360917fd5ea6769f41db2aac62d
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-09-06 19:07:27 +0300

    Add: AllRecordsQueryModel
    Created a new model - AllRecordsQueryModel, that will be used for server side searching for user records

---
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Search/AllRecordsQueryModel.cs
@@ -0,0 +1,37 @@
+﻿namespace CarCare_Companion.Core.Models.Search;
+
+using CarCare_Companion.Core.Enums;
+using CarCare_Companion.Core.Models.ServiceRecords;
+using CarCare_Companion.Core.Models.TaxRecords;
+using CarCare_Companion.Core.Models.Trip;
+
+public class AllRecordsQueryModel
+{
+    public AllRecordsQueryModel()
+    {
+        this.Categories = new HashSet<string>();
+        this.TaxRecords = new List<TaxRecordDetailsResponseModel>();
+        this.TripRecords = new List<TripDetailsByUserResponseModel>();
+        this.ServiceRecords = new List<ServiceRecordDetailsResponseModel>();
+    }
+    public const int RecordsPerPage = 10;
+
+    public int CurrentPage { get; set; } = 1;
+
+    public string? Category { get; set; }
+
+    public string? SearchTerm { get; set; }
+
+    public RecordsSorting Sorting { get; set; }
+
+    public int TotalRecords { get; set; }
+
+    public ICollection<string> Categories { get; set; }
+
+    public ICollection<TaxRecordDetailsResponseModel> TaxRecords { get; set; }
+
+    public ICollection<TripDetailsByUserResponseModel> TripRecords { get; set; }
+
+    public ICollection<ServiceRecordDetailsResponseModel> ServiceRecords { get; set; }
+
+}
