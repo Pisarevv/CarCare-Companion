be81046484dec2a09c36e7dd81c65ad615c8f619
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-09-07 17:24:05 +0300

    Create: ICostable and IOptinalCostable interfaces
    Created the interfaces - ICostable and IOptinalCostable, to be able to sort records when searching based on the interface they implement

---
+++ Server/CarCare-Companion/CarCare-Companion.Infrastructure/Data/Models/Contracts/ICostable.cs
@@ -0,0 +1,6 @@
+﻿namespace CarCare_Companion.Infrastructure.Data.Models.Contracts;
+
+public interface ICostable
+{
+    decimal Cost { get; set; }
+}
---
+++ Server/CarCare-Companion/CarCare-Companion.Infrastructure/Data/Models/Contracts/IOptionalCostable.cs
@@ -0,0 +1,6 @@
+﻿namespace CarCare_Companion.Infrastructure.Data.Models.Contracts;
+
+public interface IOptionCostable
+{
+    decimal? Cost { get; set; }
+}
