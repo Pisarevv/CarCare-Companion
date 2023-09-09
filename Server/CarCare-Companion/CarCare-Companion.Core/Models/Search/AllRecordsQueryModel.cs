acd64e510d9eed5082295b17d6cb356cddf74c46
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-09-09 18:45:21 +0300

    Update: Category property
    Changed the property from string to enum - RecordCategories

--- Server/CarCare-Companion/CarCare-Companion.Core/Models/Search/AllRecordsQueryModel.cs
+++ Server/CarCare-Companion/CarCare-Companion.Core/Models/Search/AllRecordsQueryModel.cs
@@ -9,7 +9,6 @@ public class AllRecordsQueryModel
 {
     public AllRecordsQueryModel()
     {
-        this.Categories = new HashSet<string>();
         this.TaxRecords = new List<TaxRecordDetailsResponseModel>();
         this.TripRecords = new List<TripDetailsByUserResponseModel>();
         this.ServiceRecords = new List<ServiceRecordDetailsResponseModel>();
@@ -18,15 +17,13 @@ public class AllRecordsQueryModel
 
     public int CurrentPage { get; set; } = 1;
 
-    public string? Category { get; set; }
-
+    public RecordCategories Category { get; set; }
+    
     public string? SearchTerm { get; set; }
 
     public RecordsSorting Sorting { get; set; }
 
     public int TotalRecords { get; set; }
-
-    public ICollection<string> Categories { get; set; }
 
     public ICollection<TaxRecordDetailsResponseModel> TaxRecords { get; set; }
 
