00a834f89a706ce054e6e32817f84b083a9ab0a0
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-09-09 18:44:56 +0300

    Update: Category property
    Changed the property from string to enum - RecordCategories

--- Server/CarCare-Companion/CarCare-Companion.Core/Contracts/ISearchService.cs
+++ Server/CarCare-Companion/CarCare-Companion.Core/Contracts/ISearchService.cs
@@ -12,11 +12,11 @@ public interface ISearchService
     /// Searches the records based on provided criteria.
     /// </summary>
     /// <param name="userId">The unique identifier of the user initiating the search.</param>
-    /// <param name="category">Optional. The category filter to narrow down the search results. If null, results from all categories are returned.</param>
+    /// <param name="category">The category filter to narrow down the search results.</param>
     /// <param name="searchTerm">Optional. The term to search for within records. If null, it does not filter results by term.</param>
     /// <param name="sorting">Specifies the order in which the results should be returned. Default is by newest records first.</param>
     /// <param name="currentPage">Specifies the current page number for pagination. Default is the first page.</param>
     /// <param name="recordsPerPage">Specifies the number of records to be returned per page. Default is one record per page.</param>
     /// <returns>A <see cref="RecordsQueryModel"/> containing the search results and relevant metadata.</returns>
-    public Task<RecordsQueryModel> Search(string userId, string? category = null, string? searchTerm = null, RecordsSorting sorting = RecordsSorting.Newest, int currentPage = 1, int recordsPerPage = 1);
+    public Task<RecordsQueryModel> Search(string userId, RecordCategories category = RecordCategories.All, string? searchTerm = null, RecordsSorting sorting = RecordsSorting.Newest, int currentPage = 1, int recordsPerPage = 1);
 }
