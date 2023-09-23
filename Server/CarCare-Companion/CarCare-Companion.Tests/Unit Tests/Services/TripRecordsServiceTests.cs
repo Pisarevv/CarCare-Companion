1c19a2411bbd4343433b8aeee86071efe487d5b1
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-09-23 10:39:14 +0300

    Add: Test for RetrieveTripRecordsByPageAsync and GetAllByUserIdAsQueryableAsync
    Added test for GetAllByUserIdAsQueryableAsync.
    
    Added test for RetrieveTripRecordsByPageAsync with the following scenarios:
    1. Retrieving with correct amount of records
    2. Retrieving with no trip records
    3. Retrieving with current page out of bounds

--- Server/CarCare-Companion/CarCare-Companion.Tests/Unit Tests/Services/TripRecordsServiceTests.cs
+++ Server/CarCare-Companion/CarCare-Companion.Tests/Unit Tests/Services/TripRecordsServiceTests.cs
@@ -516,6 +516,102 @@ public class TripRecordsServiceTests
 
     }
 
+
+    /// <summary>
+    /// Tests the retrieving of user trip records as queryable
+    /// </summary>
+    /// <returns></returns>
+    [Test]
+    public async Task GetAllByUserIdForSearchAsync_RetrievesUserRecords_AsQueryable()
+    {
+        //Arrange
+        ICollection<TripRecord> tripRecords = await GenerateTripRecords();
+
+        //Act
+        var result = await tripRecordsService.GetAllByUserIdAsQueryableAsync(userId);
+
+        //Assert
+        Assert.IsNotNull(result);
+        Assert.AreEqual(result.Count(), tripRecords.Count());
+    }
+
+    /// <summary>
+    /// Tests the retrieving of user trip records by page when tripRecords contains records, currentPage and recordPerPage parameters are valid.
+    /// </summary>
+    /// Disclaimer:
+    ///The service method "GetAllByUserIdAsQueryableAsync" is used to retrieve the IQueryable collection because the standard IQueryable interface does not implement IAsyncEnumerable which is required for asynchronous operations in Entity Framework Core.
+    [Test]
+    public async Task RetrieveTripRecordsByPageAsync_RetrievesCorrectAmountOfRecords()
+    {
+        //Arrange
+        ICollection<TripRecord> tripRecords = await GenerateTripRecords();
+
+        var currentPage = 2;
+        var recordPerPage = 3;
+        var expectedRecords = tripRecords.Skip((currentPage - 1) * recordPerPage).Take(recordPerPage).ToList();
+
+        //Act
+        var quaryableRecords = await tripRecordsService.GetAllByUserIdAsQueryableAsync(userId);
+        var result = await tripRecordsService.RetrieveTripRecordsByPageAsync(quaryableRecords, currentPage, recordPerPage);
+
+        //Assert
+        Assert.NotNull(result);
+        Assert.AreEqual(expectedRecords.Count, result.Count);
+        Assert.AreEqual(expectedRecords[0].StartDestination, result[0].StartDestination);
+        Assert.AreEqual(expectedRecords[1].StartDestination, result[1].StartDestination);
+        Assert.AreEqual(expectedRecords[2].StartDestination, result[2].StartDestination);
+    }
+
+    /// Tests the retrieving of user trip records by page when there are no trip records, currentPage and recordPerPage parameters are valid.
+    [Test]
+    public async Task RetrieveTripRecordsByPageAsync_EmptyList_ReturnsEmptyList()
+    {
+        // Arrange
+        var currentPage = 2;
+        var recordPerPage = 3;
+
+        // Act
+        var quaryableRecords = await tripRecordsService.GetAllByUserIdAsQueryableAsync(userId);
+        var result = await tripRecordsService.RetrieveTripRecordsByPageAsync(quaryableRecords, currentPage, recordPerPage);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsEmpty(result);
+    }
+
+    /// Tests the retrieving of user trip records by page when there are trip records but current page is out of bounds.
+    [Test]
+    public async Task RetrieveTripRecordsByPageAsync_CurrentPageOutOfBounds_ReturnsEmptyList()
+    {
+        //Arrange
+        await GenerateTripRecords();
+
+        var currentPage = 5;
+        var recordPerPage = 3;
+
+
+        // Act
+        var quaryableRecords = await tripRecordsService.GetAllByUserIdAsQueryableAsync(userId);
+        var result = await tripRecordsService.RetrieveTripRecordsByPageAsync(quaryableRecords, currentPage, recordPerPage);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsEmpty(result);
+
+    }
+
+
+    [TearDown]
+    public void TearDown()
+    {
+        applicationDbContext.Dispose();
+    }
+
+
+    /// <summary>
+    /// Generates trip records in the database.
+    /// </summary>
+    /// <returns>A collection of trip records</returns>
     private async Task<List<TripRecord>> GenerateTripRecords()
     {
         Vehicle vehicle = new Vehicle()
@@ -642,9 +738,4 @@ public class TripRecordsServiceTests
         return tripRecords;
     }
 
-    [TearDown]
-    public void TearDown()
-    {
-        applicationDbContext.Dispose();
-    }
 }
