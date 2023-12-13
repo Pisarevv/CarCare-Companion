f2029b155b1f1ee4db4186aec9502ac9cddc2448
Pisarevv <102079784+Pisarevv@users.noreply.github.com>
2023-12-13 15:48:36 +0200

    Add: docker file and docker-compose

---
+++ SqlServer2022-Tests/init.sql/init.sql
@@ -0,0 +1,5 @@
+-- Create a new database
+CREATE DATABASE CarCare-Companion-Tests;
+
+-- Use the newly created database
+USE CarCare-Companion-Tests;
---
+++ SqlServer2022/init.sql/init.sql
@@ -0,0 +1,5 @@
+-- Create a new database
+CREATE DATABASE CarCare-Companion;
+
+-- Use the newly created database
+USE CarCare-Companion;
